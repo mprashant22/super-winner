a7dacf7e6f2303a40dd938842235d8dd8a073151
prashant@acmeintech.in <prashant@acmeintech.in>
2017-05-29 11:14:55 +0530

    db connect

--- includes/db/db_connection.php
+++ includes/db/db_connection.php
@@ -11,7 +11,7 @@ class DB_Conenction {
         echo "digital ocean"; 
         echo "host >> ".DB_HOST;
         echo "user >>".DB_USER;
-        echo "pass >>".DB_NAME;
+        echo "pass >>".DB_PASS;
 //     	if (APP_ENV == "local") {
             $this->connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 //         } elseif (APP_ENV == "heroku") { echo 'heroku connect';
