b909d93ef50f635a0e0cd5485bc0ed9496e668cb
prashant@acmeintech.in <prashant@acmeintech.in>
2017-05-29 14:42:53 +0530

    db connect

--- includes/config/conf.inc.php
+++ includes/config/conf.inc.php
@@ -15,7 +15,7 @@ define("APP_ENV", "heroku");
     define("DB_PASS", "e3828e583f915c60dcaea9ed125420284a8c8fa3bb8ec463");
 //}
 
-define("SHOPIFY_API_KEY", "b0f177c128a0b6fcb23226d1132f7026");
-define("SHOPIFY_API_SECRET", "dc73e2402d1c41278c6ea6356d1d16b2");
-define("CALLBACK_URL", "https://shoppify-app-installation.herokuapp.com/install");
-define("APP_URL", "https://shoppify-app-installation.herokuapp.com/app");
+define("SHOPIFY_API_KEY", "f78660754f798a46abbecf982687199d");
+define("SHOPIFY_API_SECRET", "0749071abc7349a74cfae606a2f90ee5");
+define("CALLBACK_URL", "http://192.241.146.48/shopifyDemoLamp/install/");
+define("APP_URL", "http://product-demo-app.herokuapp.com/");
