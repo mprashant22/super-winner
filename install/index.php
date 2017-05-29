970dbcb00e5ecdb93625fb5e4f11a287562548b8
prashant@acmeintech.in <prashant@acmeintech.in>
2017-05-29 17:23:30 +0530

    db connect

--- install/index.php
+++ install/index.php
@@ -8,25 +8,24 @@ $Stores = new Stores();
 $shop = $_REQUEST['shop'];
 echo "Shop=======".$shop;
 $code = isset($_GET["code"]) ? $_GET["code"] : false;
-echo "code;;;;;;;;;;".$code;
 
-if ($shop && !$code) {echo "ohkay";
+if ($shop && !$code) {
     // validate the shopify url
     if (!$Shopify->validateMyShopifyName($shop)) {
         echo "Invalid shopify url";
     }
     
     $redirect_url = $Shopify->getAuthUrl($shop);
-    echo 'redirect url : '.$redirect_url;
-    header("Location: $redirect_url");
+    //echo 'redirect url : '.$redirect_url;
+    //header("Location: $redirect_url");
     
 }
-
-if ($code) {echo 'ohkay1';
+echo "code????????? ".$code;
+if ($code) {
     // we want to exchange the temp token passed by the shopify server during the installation process
     // in exchange of a permanent token which we need in order to get/gain access on the shopify store
     $exchange_token_response = $Shopify->exchangeTempTokenForPermanentToken($shop, $code);
-    echo 'exchange token  : '.$exchange_token_response;
+    
     // validate access token
     if(!isset($exchange_token_response->access_token) && isset($exchange_token_response->errors)) {
         // access token is not valid, redirect user to error page
@@ -40,16 +39,15 @@ if ($code) {echo 'ohkay1';
 //     echo "</pre>";
     
     $access_token = $exchange_token_response->access_token;
-    echo 'acess tokken >>'.$access_token;
+    
     if (empty($access_token)) {
         echo "Invalid access token";
     }
     
-    echo 'shoppppppppp='.$shop;
     // we check if it's a fresh installation
     $shop_info = $Stores->is_shop_exists($shop);
-    echo 'shopInfo>>'.$shop_info;
-    if (!empty($shop_info)) {
+    
+    if (empty($shop_info)) {
     	$api_key=SHOPIFY_API_KEY;
     	echo 'fresh installation of app'; // this means that's it's a fresh installation, so we do the installation process
         $Stores->addData(array(
@@ -69,7 +67,7 @@ if ($code) {echo 'ohkay1';
         ), "store_url = '$shop'");
     }
     
-    header("Location: " . APP_URL . "?shop=$shop");
+  //  header("Location: " . APP_URL . "?shop=$shop");
 }
 ?>
 
