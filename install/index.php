89eb4ceb23d60878b99240159e3a15ccc83ed63b
prashant@acmeintech.in <prashant@acmeintech.in>
2017-05-31 15:00:48 +0530

    db mein nhi jaa rha

--- install/index.php
+++ install/index.php
@@ -25,7 +25,7 @@ if ($code) {
 	// we want to exchange the temp token passed by the shopify server during the installation process
     // in exchange of a permanent token which we need in order to get/gain access on the shopify store
 	 $exchange_token_response = $Shopify->exchangeTempTokenForPermanentToken($shop, $code);
-print_r($exchange_token_response); 
+echo 'xx>>'.$exchange_token_response;
     // validate access token
     if(!isset($exchange_token_response->access_token) && isset($exchange_token_response->errors)) {
         // access token is not valid, redirect user to error page
