828d91106f9d8886ea99b4e494b97f7077cef266
prashant@acmeintech.in <prashant@acmeintech.in>
2017-05-31 17:38:26 +0530

    Inventory Page

---
+++ app/Inventory.php
@@ -0,0 +1,17 @@
+<link href="../templates/bootstrap.css" rel="stylesheet" type="text/css">
+<link href="../templates/bootstrap.min.css"  rel="stylesheet" type="text/css">
+
+<<table>
+  <tr>
+    <th>Column 1 Heading</th>
+    <th>Column 2 Heading</th>
+  </tr>
+  <tr>
+    <td>Row 1: Col 1</td>
+    <td>Row 1: Col 2</td>
+  </tr>
+</table>
+
+<?php
+
+?>
