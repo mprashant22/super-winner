17e01490d5a1ed9a04f34875651a7626500f6cd8
prashant@acmeintech.in <prashant@acmeintech.in>
2017-05-31 18:52:02 +0530

    Inventory Page

--- app/Inventory.php
+++ app/Inventory.php
@@ -1,17 +1,22 @@
 <link href="../templates/bootstrap.css" rel="stylesheet" type="text/css">
 <link href="../templates/bootstrap.min.css"  rel="stylesheet" type="text/css">
 
-<<table>
+<table>
   <tr>
-    <th>Column 1 Heading</th>
-    <th>Column 2 Heading</th>
-  </tr>
-  <tr>
-    <td>Row 1: Col 1</td>
-    <td>Row 1: Col 2</td>
-  </tr>
+    <th>Handle</th>
+    <th>Title</th>
+    <th>Variant1</th>
+    <th>Variant2</th>
+    <th>Variant3</th>
+    <th>Variant~SKU</th>
+    <th>Units</th>
+    <th>Price</th>
+   </tr>
+    
+   
 </table>
 
 <?php
 
 ?>
+<th>Variant1</th>
