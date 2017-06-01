f13094434a0ee579285e81afcdccd883f7aab923
prashant@acmeintech.in <prashant@acmeintech.in>
2017-06-01 18:01:52 +0530

    Inventory Page

--- app/Inventory.php
+++ app/Inventory.php
@@ -45,10 +45,6 @@
 
 echo 'in inventory';
 require '../includes/db/db_connection.php';
-
-$Inv = new Inventory();
-$Inv->exportExc2MySQL();
-
 class Inventory extends DB_Connection{
 	
 	private $table_name = "products";
@@ -62,42 +58,45 @@ class Inventory extends DB_Connection{
 	public function exportExc2MySQL()
 	{
 		echo 'x';
-		if(isset($_POST['SUBMIT']))
-		{
-			$fname = $_FILES['sel_file']['name'];
-			$chk_ext = explode(".",$fname);
-			if(strtolower($chk_ext[1]) == "csv")
-			{
+// 		if(isset($_POST['SUBMIT']))
+// 		{
+// 			$fname = $_FILES['sel_file']['name'];
+// 			$chk_ext = explode(".",$fname);
+// 			if(strtolower($chk_ext[1]) == "csv")
+// 			{
 		
-				$filename = $_FILES['sel_file']['tmp_name'];
-				$handle = fopen($filename, "r");
+// 				$filename = $_FILES['sel_file']['tmp_name'];
+// 				$handle = fopen($filename, "r");
 				
-				while (($data = fgetcsv($handle, 1000, ",")) !== FALSE)
-				{
-					$sql = "INSERT into products(Handle,Title,Body_HTML,Vendor,Type,Tags,Published,Option1_Name,Option1_Value,
-							Option2_Name,Option2_Value,Option3_Name,Option3_Value,Variant_SKU,Variant_Grams,Variant_Inventory_Tracker,
-							Variant_Inventory_Qty,Variant_Inventory_Policy,Variant_Fulfillment_Service,Variant_Price,
-							Variant_Compare_At_Price,Variant_Requires_Shipping,Variant_Taxable,Variant_Barcode,Image_Src,
-							Image_Position,Image_Alt_Text,Gift_Card,SEO_Title,SEO_Description,Google_Shopping_Google_Product_Category,
-							Google_Shopping_Gender,Google_Shopping_Age_Group,Google_Shopping_MPN,Google_Shopping_AdWords_Grouping,
-							Google_Shopping_AdWords_Labels,Google_Shopping_Condition,Google_Shopping_Custom_Product,
-							Google_Shopping_Custom_Label0,Google_Shopping_Custom_Label1,Google_Shopping_Custom_Label2,
-							Google_Shopping_Custom_Label3,Google_Shopping_Custom_Label4,Variant_Image,Variant_Weight_Unit,
-							Variant_Tax_Code) values('$data[0]','$data[1]','$data[2]','$data[3]','$data[4]','$data[5]','$data[6]','$data[7]','$data[8]','$data[9]','$data[10]','$data[11]','$data[12]','$data[13]','$data[14]','$data[15]','$data[16]','$data[17]','$data[18]','$data[19]','$data[20]','$data[21]','$data[22]','$data[23]','$data[24]','$data[25]','$data[26]','$data[27]','$data[28]','$data[29]','$data[30]','$data[31]','$data[32]','$data[33]','$data[34]','$data[35]','$data[36]','$data[37]','$data[38]','$data[39]','$data[40]','$data[41]','$data[42]','$data[43]','$data[44]','$data[45]')";
-					mysqli_query($sql) or die(mysqli_error());
-				}
+// 				while (($data = fgetcsv($handle, 1000, ",")) !== FALSE)
+// 				{
+// 					$sql = "INSERT into products(Handle,Title,Body_HTML,Vendor,Type,Tags,Published,Option1_Name,Option1_Value,
+// 							Option2_Name,Option2_Value,Option3_Name,Option3_Value,Variant_SKU,Variant_Grams,Variant_Inventory_Tracker,
+// 							Variant_Inventory_Qty,Variant_Inventory_Policy,Variant_Fulfillment_Service,Variant_Price,
+// 							Variant_Compare_At_Price,Variant_Requires_Shipping,Variant_Taxable,Variant_Barcode,Image_Src,
+// 							Image_Position,Image_Alt_Text,Gift_Card,SEO_Title,SEO_Description,Google_Shopping_Google_Product_Category,
+// 							Google_Shopping_Gender,Google_Shopping_Age_Group,Google_Shopping_MPN,Google_Shopping_AdWords_Grouping,
+// 							Google_Shopping_AdWords_Labels,Google_Shopping_Condition,Google_Shopping_Custom_Product,
+// 							Google_Shopping_Custom_Label0,Google_Shopping_Custom_Label1,Google_Shopping_Custom_Label2,
+// 							Google_Shopping_Custom_Label3,Google_Shopping_Custom_Label4,Variant_Image,Variant_Weight_Unit,
+// 							Variant_Tax_Code) values('$data[0]','$data[1]','$data[2]','$data[3]','$data[4]','$data[5]','$data[6]','$data[7]','$data[8]','$data[9]','$data[10]','$data[11]','$data[12]','$data[13]','$data[14]','$data[15]','$data[16]','$data[17]','$data[18]','$data[19]','$data[20]','$data[21]','$data[22]','$data[23]','$data[24]','$data[25]','$data[26]','$data[27]','$data[28]','$data[29]','$data[30]','$data[31]','$data[32]','$data[33]','$data[34]','$data[35]','$data[36]','$data[37]','$data[38]','$data[39]','$data[40]','$data[41]','$data[42]','$data[43]','$data[44]','$data[45]')";
+// 					mysqli_query($sql) or die(mysqli_error());
+// 				}
 		
-				fclose($handle);
-				echo "Successfully Imported";
-			}
-			else
-			{
-				echo "Invalid File";
-			}
-		}
+// 				fclose($handle);
+// 				echo "Successfully Imported";
+// 			}
+// 			else
+// 			{
+// 				echo "Invalid File";
+// 			}
+// 		}
 	}
 }	
+$Inv = new Inventory();
+$Inv->exportExc2MySQL();
 ?>
+
 <form action='<?php echo $_SERVER["PHP_SELF"];?>' method='post'>
 
 Import File : <input type='file' name='sel_file' size='20'>
