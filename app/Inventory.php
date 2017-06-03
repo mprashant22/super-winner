280247b885aa47700dbbfa8a4a7b9657dd6b2da2
prashant@acmeintech.in <prashant@acmeintech.in>
2017-06-03 12:38:04 +0530

    file upload

--- app/Inventory.php
+++ app/Inventory.php
@@ -61,7 +61,7 @@ class Inventory extends DB_Connection{
 		echo shell_exec("cd ..");
 		echo shell_exec("pwd");
 		echo '<pre>'.`whoami 2>&1`.'</pre>';
-		echo '<pre>'.`chmod 777 temp.txt 2>&1`.'</pre>';
+		echo '<pre>'.`chmod 777 /var/www/html/shopifyDemoLamp/temp.txt 2>&1`.'</pre>';
 		echo '<pre>'.`cat temp.txt 2>&1`.'</pre>';
 		if(isset($_POST['submit']))
 		{ 
