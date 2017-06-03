723ef8d78e2216da83406983015304d6e4c441d7
prashant@acmeintech.in <prashant@acmeintech.in>
2017-06-03 17:29:25 +0530

    file upload

--- app/Inventory.php
+++ app/Inventory.php
@@ -100,7 +100,7 @@ class Inventory extends DB_Connection{
 							Google_Shopping_AdWords_Labels,Google_Shopping_Condition,Google_Shopping_Custom_Product,
 							Google_Shopping_Custom_Label0,Google_Shopping_Custom_Label1,Google_Shopping_Custom_Label2,
 							Google_Shopping_Custom_Label3,Google_Shopping_Custom_Label4,Variant_Image,Variant_Weight_Unit,
-							Variant_Tax_Code) values($data[0],$data[1],$data[2],$data[3],$data[4],$data[5],$data[6],$data[7],$data[8],$data[9],$data[10],$data[11],$data[12],$data[13],$data[14],$data[15],$data[16],$data[17],$data[18],$data[19],$data[20],$data[21],$data[22],$data[23],$data[24],$data[25],$data[26],$data[27],$data[28],$data[29],$data[30],$data[31],$data[32],$data[33],$data[34],$data[35],$data[36],$data[37],$data[38],$data[39],$data[40],$data[41],$data[42],$data[43],$data[44],$data[45])";
+							Variant_Tax_Code) values('$data[0]','$data[1]','$data[2]','$data[3]','$data[4]','$data[5]',$data[6],$data[7],$data[8],$data[9],$data[10],$data[11],$data[12],$data[13],$data[14],$data[15],$data[16],$data[17],$data[18],$data[19],$data[20],$data[21],$data[22],$data[23],$data[24],$data[25],$data[26],$data[27],$data[28],$data[29],$data[30],$data[31],$data[32],$data[33],$data[34],$data[35],$data[36],$data[37],$data[38],$data[39],$data[40],$data[41],$data[42],$data[43],$data[44],$data[45])";
 					mysqli_query($sql) or die(mysqli_error($connection));
 				}
 				
