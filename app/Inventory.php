9377b5fad606b72298b334a0611d35f115dbe675
prashant@acmeintech.in <prashant@acmeintech.in>
2017-06-05 14:52:56 +0530

    file upload

--- app/Inventory.php
+++ app/Inventory.php
@@ -83,9 +83,9 @@ function exportExc2MySQL()
 				{
 					//$num = count($data);
 		$data_val[] = $data;
-		print_r($data_val);
-		foreach($data_val[0] as $key=>$val){
-			echo $key."is".$val;
+		//print_r($data_val);
+		foreach($data_val[0] as $val){
+			echo $val;
 		}
 // 					foreach ($data as $value){
 // 						$values[] = "'$value'";
