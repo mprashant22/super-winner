702c62bec2fc672c0b4a37c30df5968702fd7047
prashant@acmeintech.in <prashant@acmeintech.in>
2017-06-05 12:52:52 +0530

    file upload

--- app/Inventory.php
+++ app/Inventory.php
@@ -97,7 +97,8 @@ function exportExc2MySQL()
 					foreach ($data as $value){
 						$values[] = $value;
 					}
-					print_r($values);
+					$query .= "VALUES(" . implode(", ", $values) . ")";
+					echo $query;
 // 					$sql = "INSERT into products(Handle,Title,Body_HTML,Vendor) values(". implode(", ", $values).")";
 // 					echo 'sql q>>'.$sql;
 // 					mysqli_query($connection,$sql) or die(mysqli_error($connection));
