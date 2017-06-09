4703f927e4d972a5bd3af727a73af0705f0a7914
prashant@acmeintech.in <prashant@acmeintech.in>
2017-06-09 16:44:18 +0530

    cron

--- app/Inventory.php
+++ app/Inventory.php
@@ -48,14 +48,14 @@
 <input type="submit" name="submit" value="Submit"/>
 </form>
 <?php
-//if(isset($_POST['submit'])){//to run PHP script on submit
+if(isset($_POST['submit'])){//to run PHP script on submit
 if(!empty($_POST['check_list'])){
 // Loop to store and display values of individual checked checkbox.
 foreach($_POST['check_list'] as $selected){
 echo $selected."</br>";
 }
 }
-//}
+}
 ?>
 
 
