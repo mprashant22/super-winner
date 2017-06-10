0e1dbe2d3379dfd1c9797960bde91cadc46ca8ca
prashant@acmeintech.in <prashant@acmeintech.in>
2017-06-10 12:25:57 +0530

    Inventory_Stores

---
+++ app/Inventory_Stores.php
@@ -0,0 +1,41 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<form id="tbl">
+<input type="text" name="name">
+<button id="btn">submit</button>
+</form>
+
+
+
+
+ 
+<script  type="text/javascript">
+$(document).ready(function(){
+
+$("#btn").click(function(){
+
+ var myform = document.getElementById("tbl");
+  var fd = new FormData(myform );
+  $.ajax({
+     url: "Inventory.php",
+      data: fd,
+      cache: false,
+      processData: false,
+      contentType: false,
+      type: 'POST',
+      success: function (dataofconfirm) {
+         $("#demo").text("jkdhilahg");
+         alert(dataofconfirm);
+      }
+  });
+});
+
+});
+
+</script>
+
+<div id="demo">
+  ranjeet
+</div>
+
+
