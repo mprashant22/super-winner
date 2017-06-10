70ffc483b3ecdef4966e2992de981797ea99e917
prashant@acmeintech.in <prashant@acmeintech.in>
2017-06-10 17:11:44 +0530

    Inventory

--- app/Inventory.php
+++ app/Inventory.php
@@ -53,10 +53,10 @@
 function checkB()
 {
 	 var x = document.getElementById('myDIV');
-	    if (x.style.display === 'none') {
-	        x.style.display = 'block';
-	    } else {
+	    if (x.style.display === 'block') {
 	        x.style.display = 'none';
+	    } else {
+	        x.style.display = 'block';
 	    }
 	
 		//alert("hellow");
