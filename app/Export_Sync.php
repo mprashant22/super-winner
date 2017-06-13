62d99e1254c9322bf591eb98137e38ee2099044c
prashant@acmeintech.in <prashant@acmeintech.in>
2017-06-13 10:43:25 +0530

    Export n Sync

--- app/Export_Sync.php
+++ app/Export_Sync.php
@@ -127,7 +127,8 @@ public function exportExc2MySQL()
 
 <form action='<?php echo $_SERVER["PHP_SELF"];?>' method='post' enctype="multipart/form-data">
 
-Import File : <input type="hidden" name="file" value="/root/products_export.csv">
+
 <input type="submit" name="EXPORT" value="EXPORT">
+<input type="submit" name="SYNC" value="SYNC">
 
 </form>
