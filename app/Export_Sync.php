45b20873291d2832141328496c7a7b9cd8cee54e
prashant@acmeintech.in <prashant@acmeintech.in>
2017-06-13 14:03:55 +0530

    Export n Sync

--- app/Export_Sync.php
+++ app/Export_Sync.php
@@ -61,7 +61,7 @@ class Export_Sync extends DB_Connection{
 public function exportExc2MySQL()
 	{
 		echo 'csv2mysql';
-		echo $this->connection;
+		//$connection= $this->connect();
 		
 	
  		if(isset($_POST['EXPORT']))
@@ -114,7 +114,7 @@ public function exportExc2MySQL()
 		echo $table[0];
 		
 		$sql = "create table".$table[0]."(Handle text, Title text, Variant 1 text,Variant 2 text, Variant 3 text, Variant SKU integer, Variant Inventory integer, Variant Price integer)";
-		mysqli_query($connection,$sql) or die(mysqli_error($connection));		
+		mysqli_query($this->connection,$sql) or die(mysqli_error($this->connection));		
 	}
 	
 		
