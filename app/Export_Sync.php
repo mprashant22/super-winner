719c6d4104909a386d1c991deb11fe667bbc1f0f
prashant@acmeintech.in <prashant@acmeintech.in>
2017-06-13 16:17:20 +0530

    Export n Sync

--- app/Export_Sync.php
+++ app/Export_Sync.php
@@ -113,7 +113,7 @@ class Export_Sync extends DB_Connection{
 		
 		if(isset($_POST['SYNC']))
 		{ 
-			$sql = "create table ".`$table[0]`."(Handle text, Title text, `Variant 1` text,`Variant 2` text, `Variant 3` text, `Variant SKU` integer, `Variant Inventory` integer, Variant Price integer)";
+			$sql = "create table ".`$table[0]`."(Handle text, Title text, `Variant 1` text,`Variant 2` text, `Variant 3` text, `Variant SKU` integer, `Variant Inventory` integer, `Variant Price` integer)";
 			mysqli_query($this->connection,$sql) or die(mysqli_error($this->connection));
 		}
 	}
