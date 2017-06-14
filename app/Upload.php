cb7b7e38751c401daca9ec033667b04f60617b6c
prashant@acmeintech.in <prashant@acmeintech.in>
2017-06-14 18:15:16 +0530

    uploads

--- app/Upload.php
+++ app/Upload.php
@@ -8,7 +8,7 @@ $target_dir = dirname(getcwd()).DIRECTORY_SEPARATOR.'uploads'.DIRECTORY_SEPARATO
 echo 'Trdr>'.$target_dir;
 
 
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["tmp_name"]);
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 echo "trget file>><pre>".$target_file."</pre>";
 $uploadOk = 1;
 $csvFileType = pathinfo($target_file);
@@ -47,7 +47,9 @@ if(isset($_POST["submit"])) {
 // 	echo "Sorry, your file was not uploaded.";
 // 	// if everything is ok, try to upload file
 // } else {
-	move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], '/var/www/html/shopifyDemoLamp/uploads/'.basename($_FILES["fileToUpload"]["tmp_name"]));
+if(isset($_POST['submit']))
+move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], '/var/www/html/shopifyDemoLamp/uploads/'.basename($_FILES["fileToUpload"]["tmp_name"]));
+
 // 	echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
 // 	} else {
 // 		echo "Sorry, there was an error uploading your file.";
