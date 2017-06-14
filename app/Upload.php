150b699f496211033f1b7b4a5f2f73887183babf
prashant@acmeintech.in <prashant@acmeintech.in>
2017-06-14 13:09:09 +0530

    upload file

--- app/Upload.php
+++ app/Upload.php
@@ -42,6 +42,33 @@ if ($_FILES["fileToUpload"]["size"] > 500000) {
 	$uploadOk = 0;
 }
 
+
+
+
+
+
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+	echo "Sorry, your file was not uploaded.";
+	// if everything is ok, try to upload file
+} else {
+	if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+		echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+	} else {
+		echo "Sorry, there was an error uploading your file.";
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
 ?>
 		 
 		
