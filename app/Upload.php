4d564898bb2741d4d519c7f178f329da86d5dd80
prashant@acmeintech.in <prashant@acmeintech.in>
2017-06-14 18:06:08 +0530

    uploads

--- app/Upload.php
+++ app/Upload.php
@@ -3,29 +3,18 @@ echo "in upload";
 $target_dir = dirname(getcwd()).DIRECTORY_SEPARATOR.'uploads'.DIRECTORY_SEPARATOR;
 
 
-//chdir('../uploads');
+
 
 echo 'Trdr>'.$target_dir;
 
-//__DIR__=== $target_dir;
-// if(shell_exec("mkdir -p uploads 2>&1" ))
-// {
-// 	echo 'success';	
-// }
-// else
-// 	echo 'fail';
 
-// echo "<pre>".__DIR__."</pre>";
-
-
-//echo "??File>>".basename($_FILES["fileToUpload"]["name"]);
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["tmp_name"]);
 echo "trget file>><pre>".$target_file."</pre>";
 $uploadOk = 1;
 $csvFileType = pathinfo($target_file);
 echo "extension>>".$csvFileType['extension'];
 print_r($csvFileType);
-// Check if image file is a actual image or fake image
+
 if(isset($_POST["submit"])) {
 	
 	if($csvFileType['extension'])
@@ -38,10 +27,10 @@ if(isset($_POST["submit"])) {
 	}
 }
 // Check if file already exists
-if (file_exists($target_file)) {
-	echo "Sorry, file already exists.";
-	$uploadOk = 0;
-}
+// if (file_exists($target_file)) {
+// 	echo "Sorry, file already exists.";
+// 	$uploadOk = 0;
+// }
 // Check file size
 // if ($_FILES["fileToUpload"]["size"] > 500000) {
 // 	echo "Sorry, your file is too large.";
@@ -58,8 +47,8 @@ if (file_exists($target_file)) {
 // 	echo "Sorry, your file was not uploaded.";
 // 	// if everything is ok, try to upload file
 // } else {
-	move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file);
-	echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+	move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], '/var/www/html/shopifyDemoLamp/uploads/'.basename($_FILES["fileToUpload"]["tmp_name"]));
+// 	echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
 // 	} else {
 // 		echo "Sorry, there was an error uploading your file.";
 // 	}
