96a02f523c9549165985fd70ba67bc31fcfa2d32
prashant@acmeintech.in <prashant@acmeintech.in>
2017-06-19 15:44:30 +0530

    All

--- app/Export_Sync.php
+++ app/Export_Sync.php
@@ -172,4 +172,10 @@ function HandleBrowseClick()
     var fileinput = document.getElementById("browse");
     fileinput.click();
 }
+function Handlechange()
+{
+    var fileinput = document.getElementById("browse");
+    var textinput = document.getElementById("filename");
+    textinput.value = fileinput.value;
+}
 </script>
