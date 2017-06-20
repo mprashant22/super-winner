5d41c3d4f2f20134f6de013f530e248b5c39340f
prashant@acmeintech.in <prashant@acmeintech.in>
2017-06-20 11:21:18 +0530

    All

--- app/Export_Sync.php
+++ app/Export_Sync.php
@@ -74,7 +74,7 @@ class Export_Sync extends DB_Connection{
   				move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], '/var/www/html/shopifyDemoLamp/uploads/'.basename($_FILES["fileToUpload"]["name"]));
   				
   				echo '$_FILES>>'.$_FILES["fileToUpload"]["name"];
-  				echo  '>>shop>>'.$_REQUEST['shop'];
+  				echo  '>>shop>>{{'.$_REQUEST['shop']."}}";
   				
 // 		$filename = "/var/www/html/shopifyDemoLamp/uploads/products_export4.csv";
 
