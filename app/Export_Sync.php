2b67741e22517039a03c17388a6644e92be2dd40
prashant@acmeintech.in <prashant@acmeintech.in>
2017-06-20 16:53:58 +0530

    All

--- app/Export_Sync.php
+++ app/Export_Sync.php
@@ -121,25 +121,3 @@ class Export_Sync{
 
 
 ?>
-
-<!-- <form action='' method='post' enctype="multipart/form-data"> -->
-<!-- Select csv to upload: -->
-<!-- <input type="file" id="browse" name="fileToUpload" style="display: none" onChange="Handlechange();" accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel"/>-->
-<!-- <input type="text" id="filename" readonly="true"/> -->
-<!-- <input type="button" value="EXPORT" id="fileToUpload" onclick="HandleBrowseClick();"/>-->
-<!-- <input type="submit" name="SYNC" value="SYNC"> -->
-<!-- </form> -->
-
-<!-- <script> -->
-// function HandleBrowseClick()
-// {
-//     var fileinput = document.getElementById("browse");
-//     fileinput.click();
-// }
-// function Handlechange()
-// {
-//     var fileinput = document.getElementById("browse");
-//     var textinput = document.getElementById("filename");
-//     textinput.value = fileinput.value;
-// }
-<!-- </script> -->
