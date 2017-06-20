9d4cb65e18935538148a14dcc5d9a78ece07a5dc
prashant@acmeintech.in <prashant@acmeintech.in>
2017-06-20 16:02:13 +0530

    All

--- app/index.php
+++ app/index.php
@@ -8,25 +8,6 @@ require __DIR__. '../../includes/utils/Shopify.php';
 require __DIR__. '../../includes/db/Stores.php';
 require __DIR__. '../../app/Export_Sync.php';
 
-//$Shopify = new Shopify();
-//echo '$ shopify'.$Shopify;
-//$Stores = new Stores();
-//echo '$ stores'.$Stores;
-
-//$shop = $_GET['shop'];
-//$shop_info = $Stores->is_shop_exists($shop);
-
-//echo "<pre>";
-//echo 'shop_info>>'.$shop_info;
-//echo "</pre>";
-
-//echo "access token is: " . $shop_info[0]['access_token'];
-//$products = $Shopify->get_products($shop, $shop_info[0]['access_token']);
-//echo 'products>>'.$products;
-
-//echo "<pre>";
-//print_r($products->products);
-//echo "</pre>";
 
 ?>
 
