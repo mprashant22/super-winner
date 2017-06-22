a4d27d4ea375a23d94d46db7a4235e8343646ec5
prashant@acmeintech.in <prashant@acmeintech.in>
2017-06-22 13:45:28 +0530

    All

--- app/exportProduct.php
+++ app/exportProduct.php
@@ -34,15 +34,17 @@ while(! feof($handle))
 	
 	$values_csv=[];
 	$values_csv=$data_csv;
-	
+	echo "<pre>".print_r($values_csv)."</pre>";
 	
 	for ($i=0;$i<count($data);$i++)
 	{
+		echo "loop mei";
 		$data1=mysqli_escape_string($connection, $data[$i]);
 		$db.="'".$data1."',";
 	}
 	
 	$sql = "INSERT into products(Handle,Title,Body_HTML,Vendor) values(".rtrim($db,",").")";
+	echo 'sql---------'.$sql;
 	$db="";
 	mysqli_query($this->connection,$sql) or die(mysqli_error($this->connection));
 	
