382e682df986c2ee8e155115d9e523db28c320a8
prashant@acmeintech.in <prashant@acmeintech.in>
2017-06-22 12:12:00 +0530

    All

--- app/Export_Sync.php
+++ app/Export_Sync.php
@@ -56,24 +56,24 @@ class Export_Sync extends DB_Connection{
 	public function sync()
 	{
 	
-		echo 'csv2mysql';
+// 		echo 'csv2mysql';
 
  		
- 			$target_dir = dirname(getcwd()).DIRECTORY_SEPARATOR.'uploads'.DIRECTORY_SEPARATOR;
+//  			$target_dir = dirname(getcwd()).DIRECTORY_SEPARATOR.'uploads'.DIRECTORY_SEPARATOR;
  			
- 			echo 'Trdr>'.$target_dir;
+//  			echo 'Trdr>'.$target_dir;
  			
  			
- 			$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-  			echo "trget file>><pre>".$target_file."</pre>";
- 			$uploadOk = 1;
- 			$csvFileType = pathinfo($target_file);
-  			echo "extension>>".$csvFileType['extension'];
-  			print_r($csvFileType);
+//  			$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+//   			echo "trget file>><pre>".$target_file."</pre>";
+//  			$uploadOk = 1;
+//  			$csvFileType = pathinfo($target_file);
+//   			echo "extension>>".$csvFileType['extension'];
+//   			print_r($csvFileType);
  			
-  				move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], '/var/www/html/shopifyDemoLamp/uploads/'.basename($_FILES["fileToUpload"]["name"]));
+//   				move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], '/var/www/html/shopifyDemoLamp/uploads/'.basename($_FILES["fileToUpload"]["name"]));
   				
-  				echo '$_FILES>>'.$_FILES["fileToUpload"]["name"];
+//   				echo '$_FILES>>'.$_FILES["fileToUpload"]["name"];
   				
   				
  		$filename = "/var/www/html/shopifyDemoLamp/uploads/products_export4.csv";
