057bd8dd3400a700d855089c24bbec8816436476
prashant@acmeintech.in <prashant@acmeintech.in>
2017-06-27 16:45:04 +0530

    store table

--- app/store_table.php
+++ app/store_table.php
@@ -15,7 +15,7 @@ class StoreTable extends DB_Connection{
 	{
 		$shop='mathurs-storezz.myshopify.com';
 		$shp=explode('.', $shop);
-		$sql = "select * from "."`".$shp[0]."` order by handle";
+		$sql = "select * from "."`".$shp[0]."` order by handle group by vendor";
 		echo "sql>>".$sql."<br>";
 		$res=mysqli_query($this->connection,$sql);
 		//echo "<pre>".print_r($res)."</pre>";// or die(mysqli_error($this->connection));		
