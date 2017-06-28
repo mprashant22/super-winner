66a25506ba33ff64650bb78242a7a1cc0a0ffa33
prashant@acmeintech.in <prashant@acmeintech.in>
2017-06-28 11:36:07 +0530

    store table

--- app/Export_Sync.php
+++ app/Export_Sync.php
@@ -118,9 +118,10 @@ class Export_Sync extends DB_Connection{
  					$values_csv=[];
  					$values_csv=$data_csv;
 
-
+echo print_r($data_csv);
  					for ($i=0;$i<count($data_csv);$i++)
  					{
+ 						echo "inside LOOOOOOP";
  						$data1=mysqli_escape_string($this->connection, $data_csv[$i]);
  						$db.="'".$data1."',";
  					}
