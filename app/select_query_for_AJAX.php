2deb966a8f04109acbbdf65ab5e53369b9792a45
prashant@acmeintech.in <prashant@acmeintech.in>
2017-07-03 18:56:42 +0530

    ajax&JQuery&selectQ

--- app/select_query_for_AJAX.php
+++ app/select_query_for_AJAX.php
@@ -1,6 +1,21 @@
 <?php
-$varient1 = $_POST["variant1"];
-$varient1 = $_POST["variant2"];
-$varient1 = $_POST["variant3"];
+require_once '/var/www/html/shopifyDemoLamp/includes/db/db_connection.php';
+$variant1 = $_POST["variant1"];
+$variant2 = $_POST["variant2"];
+$variant3 = $_POST["variant3"];
+
+
+
+$shop='mathurs-storezz.myshopify.com';
+$shp=explode('.', $shop);
+
+$sql4 = "select * from "."`".$shp[0]."` where `Option1 Value` like ".$variant1;
+echo "sql>>".$sql4."<br>";
+$res4=mysqli_query($this->connection,$sql4);
+
+while($result = mysqli_fetch_assoc($res4))
+{
+	echo "sku>>".$result["Variant SKU"];
+}
 
 ?>		
