82428e0e4e6c9e9ce24a06413ebba1883ff3abd0
prashant@acmeintech.in <prashant@acmeintech.in>
2017-07-03 18:58:28 +0530

    ajax&JQuery&selectQ

--- app/select_query_for_AJAX.php
+++ app/select_query_for_AJAX.php
@@ -4,18 +4,19 @@ $variant1 = $_POST["variant1"];
 $variant2 = $_POST["variant2"];
 $variant3 = $_POST["variant3"];
 
+echo $variant1;
 
 
-$shop='mathurs-storezz.myshopify.com';
-$shp=explode('.', $shop);
+// $shop='mathurs-storezz.myshopify.com';
+// $shp=explode('.', $shop);
 
-$sql4 = "select * from "."`".$shp[0]."` where `Option1 Value` like ".$variant1;
-echo "sql>>".$sql4."<br>";
-$res4=mysqli_query($this->connection,$sql4);
+// $sql4 = "select * from "."`".$shp[0]."` where `Option1 Value` like ".$variant1;
+// echo "sql>>".$sql4."<br>";
+// $res4=mysqli_query($this->connection,$sql4);
 
-while($result = mysqli_fetch_assoc($res4))
-{
-	echo "sku>>".$result["Variant SKU"];
-}
+// while($result = mysqli_fetch_assoc($res4))
+// {
+// 	echo "sku>>".$result["Variant SKU"];
+// }
 
 ?>		
