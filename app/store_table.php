4a9976e09debf79fb17ad34000a48090939e8bbe
prashant@acmeintech.in <prashant@acmeintech.in>
2017-07-04 15:25:42 +0530

    ajax&JQuery&selectQ

--- app/store_table.php
+++ app/store_table.php
@@ -162,7 +162,7 @@ class StoreTable extends DB_Connection{
                        var ajax_unit = $("#ajax_unit").text();
                        var ajax_price = $("#ajax_price").text();
                         
-                       $(this).parent("td").siblings().find(".sku").text("rrr");
+                       $(this).parent("td").nexts(".price").text("rrr");
                        //$(this).parent("td").siblings(".units").html(ajax_unit);
                        //$(this).parent("td").siblings(".price").html(ajax_price);
                        
