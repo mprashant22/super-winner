7893ce2aa60d3c51b077f28a9e99673ff4611afe
prashant@acmeintech.in <prashant@acmeintech.in>
2017-07-04 17:21:21 +0530

    ajax&JQuery&selectQ

--- app/store_table.php
+++ app/store_table.php
@@ -34,10 +34,7 @@ class StoreTable extends DB_Connection{
      <th>Units</th>
      <th>Price</th>
     </tr>
-		<?php 
-		
-		
-	
+		<?php		
 			 while($result = mysqli_fetch_assoc($res4)) {
  
 			 	$sql1 = "select distinct(`Option1 Value`) from "."`".$shp[0]."` where handle like '".$result['handle']."' order by `Option1 Value` ASC";
@@ -49,10 +46,8 @@ class StoreTable extends DB_Connection{
 			 	$sql3 = "select distinct(`Option3 Value`) from "."`".$shp[0]."` where handle like '".$result['handle']."' order by `Option3 Value` ASC";
 			 	$res3 = mysqli_query($this->connection,$sql3);
 			 	
-			 	
     	 ?>
-
-   
+  
     <tr>
 		<td class="handle"><?php print_r($result['handle']); ?></td>
      	<td>Title</td>
@@ -95,24 +90,16 @@ class StoreTable extends DB_Connection{
  			?>
    		</select> 
    		</td> 
- 		<td>
-           <span class="sku">sku</span>
- 		</td>
-    	<td ><span class="units">Units</span></td>
-    	<td ><span class="price">Price</span></td>
-	</tr>
-	
-	
+ 		<td><span class="sku">sku</span></td>
+    	<td><span class="units">Units</span></td>
+    	<td><span class="price">Price</span></td>
+	</tr>	
 	<?php 
  	}
 	?>	
- </table> 
-
+ </table>
  </form>
 </div>
-
-
-<div id=info></div>
   
    <?php 
   
@@ -122,55 +109,49 @@ class StoreTable extends DB_Connection{
 	$obj->storeDisplay();
 ?>
 
-
-
-
 	<script type="text/javascript">
 		$(document).ready(function() {
+
+function myQuery(){	
+
+	  var handle = $(this).parent("td").siblings(".handle").text();
+      var v1 = $(this).parents("tr").find(".v1").val();
+      var v2 = $(this).parents("tr").find(".v2").val();
+      var v3 = $(this).parents("tr").find(".v3").val();
+
+      var ourObj = {};
+      ourObj.handle = handle;
+      ourObj.v1 = v1;
+      ourObj.v2 = v2;
+      ourObj.v3 = v3;
+      ourObj.arPoints = [{'x':handle, 'y': v1 , 'z':v2, 'p':v3}];
+      var $t = $(this);
+      $.ajax({
+         url: 'select_query_for_AJAX.php',
+         type: 'post',
+         data: {"points" : JSON.stringify(ourObj)},
+         success: function(data) {
+             
+              $("#ajax_success").html(data);
+              var ajax_sku = $("#ajax_sku").html();
+              var ajax_unit = $("#ajax_unit").text();
+              var ajax_price = $("#ajax_price").text();
+
+
+              $t.parents("tr").find(".price").text(ajax_price);
+              $t.parents("tr").find(".sku").text(ajax_sku);
+              $t.parents("tr").find(".units").text(ajax_unit);                       
+         }
+      });                   
+	}
 
 			disen = function(s){ // disable, enable
 				$(s).prop('disabled', function(i, v) { return !v; });
 				console.log('disable/enable button');
 			};
 
-			$( "#my_form tr td select" ).change( function() {
-               var handle = $(this).parent("td").siblings(".handle").text();
-               var v1 = $(this).parents("tr").find(".v1").val();
-               var v2 = $(this).parents("tr").find(".v2").val();
-               var v3 = $(this).parents("tr").find(".v3").val();
-
-
-               var ourObj = {};
-               ourObj.handle = handle;
-               ourObj.v1 = v1;
-               ourObj.v2 = v2;
-               ourObj.v3 = v3;
-               ourObj.arPoints = [{'x':handle, 'y': v1 , 'z':v2, 'p':v3}];
-               var $t = $(this);
-               $.ajax({
-                  url: 'select_query_for_AJAX.php',
-                  type: 'post',
-                  data: {"points" : JSON.stringify(ourObj)},
-                  success: function(data) {
-                      
-                       $("#ajax_success").html(data);
-                       var ajax_sku = $("#ajax_sku").html();
-                       var ajax_unit = $("#ajax_unit").text();
-                       var ajax_price = $("#ajax_price").text();
-
-
-                       $t.parents("tr").find(".price").text(ajax_price);
-                       $t.parents("tr").find(".sku").text(ajax_sku);
-                       $t.parents("tr").find(".units").text(ajax_unit);                       
-                  }
-               });   
-
-              
-
-
-
-               
-			});
+			$( "#my_form tr td select" ).change(function() {
+				myQuery();   
 		});
 		</script>
  		<div id="ajax_success" style="visibility: hidden"></div>
