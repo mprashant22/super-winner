cd66234a1a7c39571887ebc2d2ec2a770364e549
prashant@acmeintech.in <prashant@acmeintech.in>
2017-07-05 16:18:54 +0530

    ajax&JQuery&selectQ

--- app/Export_Sync.php
+++ app/Export_Sync.php
@@ -82,7 +82,10 @@ echo print_r($data_csv);
  						//echo "inside LOOOOOOP";
  						$data1=mysqli_escape_string($this->connection, $data_csv[$i]);
  						if(isset($data1) === false && empty($data1) === true && is_numeric($data1))
+ 						{
+ 							echo "ppppppp";
  							$data1=0;
+ 						}
  							
  						echo '[data1>>'.$data1;
  						$db.="'".$data1."',";
