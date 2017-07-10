54bfc520d64aea65f501081d330a7b3bcafd1057
prashant@acmeintech.in <prashant@acmeintech.in>
2017-07-10 17:44:50 +0530

    delete

--- app/delete.php
+++ app/delete.php
@@ -19,10 +19,10 @@ class DeleteQuery extends DB_Connection
 			//$handle=[];
 			$handle=$_POST["points"];
 			echo $handle;
-			
+			echo var_dump(gettype($handle));
 			foreach($value as $handle)
   			{
-  				echo $value."<br>";
+  				//echo $value."<br>";
 			
 //   				$sql4 = "delete from "."`".$shp[0]."` where `handle` LIKE '".$value."'";
 //   			echo "sql>>".$sql4."<br>";
