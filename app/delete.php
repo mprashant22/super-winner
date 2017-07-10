d8a5c442ee64a98c638567b27b817c1a3d085144
prashant@acmeintech.in <prashant@acmeintech.in>
2017-07-10 11:37:25 +0530

    delete

--- app/delete.php
+++ app/delete.php
@@ -2,7 +2,7 @@
 require_once '/var/www/html/shopifyDemoLamp/includes/db/db_connection.php';
 if(isset($_POST["val"]))
 {
-$id=$_GET['val'];
+$id=$_POST["val"];
 echo "id==".$id;
 $query1=pg_query("delete from store where storeid='$id'");
 if($query1)
