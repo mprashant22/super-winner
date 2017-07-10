192c0faa8aab0630c99a12712626f4619dc898df
prashant@acmeintech.in <prashant@acmeintech.in>
2017-07-10 12:27:58 +0530

    delete

--- app/store_table.php
+++ app/store_table.php
@@ -163,7 +163,7 @@ class StoreTable extends DB_Connection{
 									type: "POST",  
 									url: "delete.php",  
 									cache:false,  
-									data:"{'yy':"+n+"}",
+									data:"{'yy':'"+n+"'}",
 									//datatype:json,  
 									success: function(response)  
 									{   alert("respnse"+response);
