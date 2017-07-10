687cae6b5e45e6f5421f51b75e2439fff84228e1
prashant@acmeintech.in <prashant@acmeintech.in>
2017-07-10 17:01:39 +0530

    delete

--- app/delete.php
+++ app/delete.php
@@ -1,5 +1,38 @@
 <?php
 require_once '/var/www/html/shopifyDemoLamp/includes/db/db_connection.php';
-echo $_POST["points"];
+
+class DeleteQuery extends DB_Connection
+{
+	function __construct(){
+		$this->connection = $this->connect();
+	}
+	
+	public function ajaxResponse()
+	{
+		
+		$shop='mathurs-storezz.myshopify.com';
+		$shp=explode('.', $shop);
+		echo $shp[0];
+		// Test if our data came through
+		if (isset($_POST["points"])) {
+			
+			$handle=[];
+			$handle=$_POST["points"];
+			
+			for($i=0;$i<count($handle);$i++)
+			{			
+			
+			$sql4 = "delete from "."`".$shp[0]."` where `handle` LIKE '".$handle[$i]."'";
+			echo "sql>>".$sql4."<br>";
+// 			$res4=mysqli_query($this->connection,$sql4);
+// 			// 		 	echo $res4;
+			}
+					 	
+		}
+	}
+}
+
+$obj = new DeleteQuery();
+$obj->ajaxResponse();
 
 ?>
