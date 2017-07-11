626ed82a876787ea2a78b68aec7d0935f521abca
prashant@acmeintech.in <prashant@acmeintech.in>
2017-07-11 15:58:20 +0530

    delete

--- app/store_table.php
+++ app/store_table.php
@@ -185,20 +185,20 @@ class StoreTable extends DB_Connection{
 					var check = confirm(WRN_PROFILE_DELETE);  
 					var handle=$(".remove-row").attr("data-handle");
 					alert("handle=="+handle);
-					if(check == true){alert("true-remove row");
-					$.ajax({
-		                  url: 'delete.php',
-		                  type: 'post',
-		                  //data: {points : handle},
-		                  //success: function(data) {
-		                      alert("data>>"+data);                      						
-								$("#msgdiv").html(data);
-								location.reload();
-		                  }
-		});
-						$('table tr').filter($(this).attr("data-handle")).remove();
-					}
-			});			
+					//if(check == true){alert("true-remove row");
+// 					$.ajax({
+// 		                  url: 'delete.php',
+// 		                  type: 'post',
+// 		                  //data: {points : handle},
+// 		                  //success: function(data) {
+// 		                      alert("data>>"+data);                      						
+// 								$("#msgdiv").html(data);
+// 								location.reload();
+// 		                  }
+		//});
+// 						$('table tr').filter($(this).attr("data-handle")).remove();
+// 					}
+			});
 
 			var $rows = $('#store-table tr');
 			$('#search').keyup(function() {
