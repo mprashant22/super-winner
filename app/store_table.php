5b5cfb660f54a10e0b208a6b37fc54e0854be0f9
prashant@acmeintech.in <prashant@acmeintech.in>
2017-07-12 15:41:04 +0530

    updateANDdelete

--- app/store_table.php
+++ app/store_table.php
@@ -202,7 +202,7 @@ class StoreTable extends DB_Connection{
 
 			 $('td.editable-col').on('focusout', function() {
 				    data = {};
-// 				    data['val'] = $(this).text();
+				    data['val'] = $(this).text();
 // 				    alert("data_val>>>>>"+data['val']):
 // 				    data['id'] = $(this).parent('tr').attr('data-row-id');
 // 				    data['index'] = $(this).attr('col-index');
