bcac33ba7ce90d56b3a6a638214313f5b3d6276a
prashant@acmeintech.in <prashant@acmeintech.in>
2017-07-13 19:10:45 +0530

    update

--- app/store_table.php
+++ app/store_table.php
@@ -201,7 +201,7 @@ class StoreTable extends DB_Connection{
 			});
 
 			 $('td.editable-col').on('focusout', function() {
-$se=$("option:selected",'td').find('.v1').text(); 
+$se=$("option:selected",'td.v1').text(); 
 				    data = [];
 				    data[0] = $(this).text();
 				    data[1] = $(this).attr("data-handle");
