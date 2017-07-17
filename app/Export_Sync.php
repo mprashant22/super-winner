f367008908ccaf4faca54780adf512fb7bd12f8e
prashant@acmeintech.in <prashant@acmeintech.in>
2017-07-17 16:42:44 +0530

    update

--- app/Export_Sync.php
+++ app/Export_Sync.php
@@ -37,6 +37,7 @@ class Export_Sync extends DB_Connection{
 
 		$handle = fopen($filename, "r");
 		print_r($handle);
+		echo "<pre>".count($handle)."</pre>";
  				
 		$data_csv = fgetcsv($handle);
 		print_r($data_csv);
