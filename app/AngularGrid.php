1eff1d3a212e81cc3a162a0c995e57c1546f6f7b
prashant@acmeintech.in <prashant@acmeintech.in>
2017-07-19 17:02:35 +0530

    angularJS

--- app/AngularGrid.php
+++ app/AngularGrid.php
@@ -264,4 +264,5 @@ app.controller("myCtrl", function($scope) {
 <script type='text/javascript' src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script> 
 <script type='text/javascript' src="https://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
 </body>
+
 </html>
