96a72820bfac67cb1b86ee8443aafce10dc116e6
prashant@acmeintech.in <prashant@acmeintech.in>
2017-07-19 11:04:49 +0530

    angularJS

--- app/AngularPagination.php
+++ app/AngularPagination.php
@@ -1,3 +1,5 @@
+       <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
        <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js"></script>
         <div ng-app="myApp" ng-controller="ctrlRead">
            
