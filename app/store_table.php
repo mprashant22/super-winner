10823c6b509937882c522c2ea9a699e9047b2848
prashant@acmeintech.in <prashant@acmeintech.in>
2017-07-20 10:53:59 +0530

    angularJS

--- app/store_table.php
+++ app/store_table.php
@@ -1,24 +1,7 @@
 <script src="//code.jquery.com/jquery-2.1.1.min.js"></script>
-<!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script> -->
-<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"> -->
-<!-- <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js"></script> -->
-<link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
-<link rel="stylesheet" href="css/style.css" type="text/css">
-<link rel="stylesheet" href="font-awesome/css/font-awesome.css" type="text/css">
-<link href="https://fonts.googleapis.com/css?family=Philosopher:400,400i,700,700i" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
-<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.1/angular.min.js"></script>
-
-
-
-<script>
-var app = angular.module('storeApp', []);
-app.controller('storeCtrl', function($scope, $http) {
-	alert("inside controller");
-    $http.get("https://www.w3schools.com/angular/customers.php")
-    .then(function (response) {$scope.names = response.data.records;});
-});
-</script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js"></script>
 
 <?php
 
