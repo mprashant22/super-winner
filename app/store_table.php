c03e40b936d8b00de0f86eb7cc5211386f01bc71
prashant@acmeintech.in <prashant@acmeintech.in>
2017-07-20 19:01:37 +0530

    angularJS

--- app/store_table.php
+++ app/store_table.php
@@ -105,7 +105,7 @@ class StoreTable extends DB_Connection{
 <!--  Prashant -->
 
 
-<div ng-controller="myCtrl">
+<div ng-controller="myCtrl" ng-app="myApp">
 <input type="text" id="search" placeholder="Type to search">
 <button class="btn btn-primary delete_all">DELETE</button></div>
 <div class="col-md-3">
