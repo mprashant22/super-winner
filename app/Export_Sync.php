f16120e9e9b9f998ecbfeab744e08691688cec0e
prashant@acmeintech.in <prashant@acmeintech.in>
2017-07-24 16:03:20 +0530

    angularjs

--- app/Export_Sync.php
+++ app/Export_Sync.php
@@ -47,7 +47,7 @@ class Export_Sync extends DB_Connection{
 		while(! feof($handle))
 		{
 				echo "<pre style='color:BLUE'><h4>".++$t."</h4></pre>";
-				if(feof($handle)=='TRUE')
+				if(feof($handle)=='FALSE')
 				{
 					echo 'break';
 					break; 
