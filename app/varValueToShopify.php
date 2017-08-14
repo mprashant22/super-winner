441a953a116b3773b1f0baa4e121609c06e07e89
prashant@acmeintech.in <prashant@acmeintech.in>
2017-08-14 18:52:19 +0530

    phpToShopify

--- app/varValueToShopify.php
+++ app/varValueToShopify.php
@@ -1,17 +1,40 @@
 <?php
-echo "prashant";
-$mycustom = $shopify('GET /admin/themes/143487233/assets.json?asset[key]=layout/theme.liquid&theme_id=143487233');
-$me = $mycustom['value'];
-echo $me;
 
-?>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js" type="text/javascript"></script>
-<script>	
-	$(document).ready(function(){
-		alert("1");	
-	var current_theme =  "<?php echo $me; ?>"; //possibly from the get asset var. 
-	//alert(
-    var new_theme = jQuery(current_theme).find('body').append('<h1>Hello</h1>');
-	//console.log(new_theme);	
-})
-	</script>
+require 'libs/shopify.php';
+define('SHOPIFY_API_KEY', '4bf32fb06e5fc1b2dbde39e17690a20e');
+define('SHOPIFY_SECRET', 'XXXX');
+define('SHOPIFY_SHOP', 'nmsshop.myshopify.com');
+
+$variant = array(
+		'sku' => 'tieRed',
+		'id' => 1109059849,
+		'title' => 'Red',
+		'inventory_quantity'=>'17'
+);
+$product = array(
+		'id' =>  422795077,
+		'variants' => array($variant)
+);
+$json['product'] = $product;
+
+$stock = json_encode($json);
+
+$sc = new ShopifyClient(SHOPIFY_SHOP, 'XXXX', SHOPIFY_API_KEY, SHOPIFY_SECRET);
+
+try {
+	$put = $sc->call('PUT', '/admin/products/#422795077.json',$stock);
+	if ($put > 0) {
+		echo $stockVal . ': success';
+	} else {
+		echo 'fail';
+	}
+} catch (ShopifyApiException $e)
+{
+	echo 'API fail';
+	print_r($e);
+}
+catch (ShopifyCurlException $e)
+{
+	echo 'Curl fail (PUT)';
+	print_r($e);
+}
