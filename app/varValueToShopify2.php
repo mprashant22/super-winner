41c78dc752f8040e78bfbda447ca06fc771afe98
prashant@acmeintech.in <prashant@acmeintech.in>
2017-08-17 12:37:37 +0530

    phpToShopify

--- app/varValueToShopify2.php
+++ app/varValueToShopify2.php
@@ -107,7 +107,7 @@ $theme_id = '143487233';
 	//$last_sync = '2016-09-21T09:25:26-05:00';
 	$new_last_updated_at = 0;
 	// run a query to pull each asset in the theme
-	$assets = get_data('/admin/themes/'.$theme_id.'/assets.json', $api_key, $password, $store_url, $theme_id);
+	$assets = get_data('/admin/themes/'.$theme_id.'/assets.json?asset[key]=snippets/new_file.liquid&theme_id='.$theme_id, $api_key, $password, $store_url, $theme_id);
 	$updated_assets = [];
 	// iterate through the assets
 	foreach ($assets->assets as $key => $asset)
