ee4db423592221a9cbf33e9161eb2dcaf2946248
prashant@acmeintech.in <prashant@acmeintech.in>
2017-08-17 11:03:02 +0530

    phpToShopify

--- app/varValueToShopify2.php
+++ app/varValueToShopify2.php
@@ -74,7 +74,7 @@ $theme_id = '143487233';
 		echo "`";
 		$data['asset']['key'] = 'assets/last_sync.liquid';
 		$data['asset']['value'] = $last_sync;
-		//print_r($data);
+		print_r($data);
 		$data = json_encode($data);
 		print_r($data);
 		$response = put_data('/admin/themes/'.$theme_id.'/assets.json', $data, $api_key, $password, $store_url, $theme_id);
