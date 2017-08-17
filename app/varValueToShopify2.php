c79e49d07b06f5ea86f5b9994220aab71e1c569c
prashant@acmeintech.in <prashant@acmeintech.in>
2017-08-17 12:49:56 +0530

    phpToShopify

--- app/varValueToShopify2.php
+++ app/varValueToShopify2.php
@@ -109,6 +109,7 @@ $theme_id = '143487233';
 	// run a query to pull each asset in the theme
 	$assets = get_data('/admin/themes/'.$theme_id.'/assets.json?asset[key]=snippets/new_file1.liquid&theme_id='.$theme_id, $api_key, $password, $store_url, $theme_id);
 	$updated_assets = [];
+	print_r($assets);
 	// iterate through the assets
 	foreach ($assets->assets as $key => $asset)
 	{
@@ -132,7 +133,7 @@ $theme_id = '143487233';
 			else
 			{
 				// this is a text file of some sort. since it doesn't have a public url, we can't cURL it so the solution is to get the updated value of the file and overwrite the file in the local file structure
-				$response = get_data('/admin/themes/'.$theme_id.'/assets.json?asset[key]='.$file_name.'&theme_id='.$theme_id, $api_key, $password, $store_url, $theme_id);
+				$response = get_data('/admin/themes/'.$theme_id.'/assets.json?asset[key]=snippets/new_file1.liquid'.'&theme_id='.$theme_id, $api_key, $password, $store_url, $theme_id);
 				print_r($response);
 				file_put_contents($file_name, $response->asset->value);		    	
 			}
