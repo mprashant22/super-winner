6af850dcd4280de3082c9d90e76456bc5a03b2a9
prashant@acmeintech.in <prashant@acmeintech.in>
2017-08-19 16:02:35 +0530

    phpToShopify

--- app/smartCollection.php
+++ app/smartCollection.php
@@ -8,28 +8,18 @@
 echo $text."MATHUR";
 // Update these variables with the correct values from a new *Private* app in Shopify
 $api_key = '731148aba4b8f20f0d72c25e0a884f8b';
-$password = '00f7d6f7ad1d8649bd7bc855d2caff6e';
+$access_token= '00f7d6f7ad1d8649bd7bc855d2caff6e';
 $store_url = 'newtest-18.myshopify.com';
 
 $collection_id='345548033';
 
-	function getAuthUrl($shop)
-	{	
-		$shp=explode('.', $shop);
-		$scopes = ["read_products", "read_orders","write_orders","write_products","read_smart_collections", "write_smart_collections"];
+curlPutRequest($store_url,$api_key,$access_token);
+function curlPutRequest($store_url,$api_key,$access_token) {
 		
-		return 'https://' . $shop . '/admin/oauth/authorize?'
-				. 'scope=' . implode("%2C", $scopes)
-				. '&client_id=' . $api_key
-				. '&redirect_uri=' . CALLBACK_URL;
-	}
-
-	function curlPutRequest($url,$password,$api_key,$access_token= false) {
+		echo "curlPut";			
+		$url = 'https://' . $api_key . ':' . $access_token. '@' . $store_url.'/admin/smart_collections/order.json?collection_id=345548033';
+		echo $url;
 		
-		echo "curlPut";
-		//$url = 'https://' . $api_key . ':' . $password . '@' . $store_url;
-		//echo $url;
-		$url =  $url.$request;
 		$ch = curl_init(); //create a new cURL resource handle
 		curl_setopt($ch, CURLOPT_URL, $url); // Set URL to download
 		
