951122462d0bdfe286d2cef8976a680f2d958e63
prashant@acmeintech.in <prashant@acmeintech.in>
2017-08-19 15:37:54 +0530

    phpToShopify

--- app/smartCollection.php
+++ app/smartCollection.php
@@ -24,138 +24,46 @@ $collection_id='345548033';
 				. '&redirect_uri=' . CALLBACK_URL;
 	}
 
-	// get_data retrives data with the API
-	function get_data($request, $api_key, $password, $store_url, $collection_id)
-	{
-		getAuthUrl($store_url);
-		//echo "getData";
-		$url = 'https://' . $api_key . ':' . $password . '@' . $store_url;
-		echo $url;
-		$url =  $url.$request;
-		$session = curl_init();
-		curl_setopt($session, CURLOPT_URL, $url);
-		curl_setopt($session, CURLOPT_HTTPGET, 1); 
-		curl_setopt($session, CURLOPT_HEADER, false);
-		curl_setopt($session, CURLOPT_HTTPHEADER, array('Accept: application/json', 'Content-Type: application/json'));
-		curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($session,CURLOPT_SSL_VERIFYPEER,false);
-		$response = curl_exec($session);
-		curl_close($session);
-		$response = json_decode($response);
-		//print_r($response);
-		return $response;
-	}
-	// put data updates or uploads data with the API
-	function put_data($request, $api_key, $password, $store_url, $collection_id)
-	{
-		echo "putData";
-		$url = 'https://' . $api_key . ':' . $password . '@' . $store_url;
-		//echo $url;
-		$url =  $url.$request;
-		//echo $url;
-		$session = curl_init();
-		curl_setopt($session, CURLOPT_URL, $url);
-		curl_setopt($session, CURLOPT_HEADER, false);
-		curl_setopt($session, CURLOPT_HTTPHEADER, array('Accept: application/json', 'Content-Type: application/json'));
-        curl_setopt($session, CURLOPT_CUSTOMREQUEST, "PUT");
-        curl_setopt($session, CURLOPT_POSTFIELDS,$data);
-		curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($session,CURLOPT_SSL_VERIFYPEER,false);
-		$response = curl_exec($session);
-		curl_close($session);
-		$response = json_decode($response);
-		print_r($response);
-		return $response;
-	}
-	// returns the timestamp of the last sync
-	function get_last_sync($api_key, $password, $store_url, $collection_id)
-	{
-		//echo "get_Last_Sync";
-		$response = get_data('/admin/smart_collections/'.$collection_id.'/assets.json?asset[key]=snippets/new_file1.liquid&theme_id='.$collection_id, $api_key, $password, $store_url, $collection_id);
-		//echo "Last SYYYYYYNC".$response;
-		return $response->asset->value;
-	}
-	// writes new timestamp to the last sync file (on shopify)
-	function update_last_sync($last_sync, $api_key, $password, $store_url, $collection_id)
-	{
-		//echo "`";
-		//$data['asset']['key'] = 'snippets/new_file1.liquid';
-		//$data['asset']['value'] = "something123";
-		//print_r($data);
-		//$data = json_encode($data);
-		//print_r($data);
-			echo "response";
-			$text=$_POST['snippetText'];
-			$response = put_data('/admin/smart_collections/'.$collection_id.'/order.json', $api_key, $password, $store_url, $collection_id);
+	function curlPutRequest($url,$password,$api_key,$access_token= false) {
 		
-		//print_r($response);
-	}
-	// download a file from the shopify server. this only works for images!
-    function get_file($url){
-    	echo "getFile";
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_VERBOSE, 1);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_AUTOREFERER, false);
-        curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
-        curl_setopt($ch, CURLOPT_HEADER, 0);
-        $result = curl_exec($ch);
-        curl_close($ch);
-        return($result);
-    }
-    // using a temp file we created using get_file, write the file to the local file structure
-    function write_file($text, $new_filename){
-    	echo "write_file";
-        $fp = fopen($new_filename, 'w+');
-        fwrite($fp, $text);
-        fclose($fp);
-    }
-    // get the timestamp of the last sync so we can compare with the files being pulled
-	$last_sync = get_last_sync($api_key, $password, $store_url, $collection_id);
-	//override for testing:
-	//$last_sync = '2016-09-21T09:25:26-05:00';
-	$new_last_updated_at = 0;
-	// run a query to pull each asset in the theme
-	$assets = get_data('/admin/smart_collections/'.$collection_id.'/assets.json?asset[key]=snippets/new_file1.liquid&theme_id='.$collection_id, $api_key, $password, $store_url, $collection_id);
-	$updated_assets = [];
-	print_r($assets);
-	// iterate through the assets
-	foreach ($assets->assets as $key => $asset)
-	{
-		// check to see if the updated date on shopify is greater than the last sync date
-		$updated_at = $asset->updated_at;
-		if ($updated_at > $last_sync)
-		{
-			if ($updated_at > $new_last_updated_at)
-			{
-				$new_last_updated_at = $updated_at;
-			}
-			$file_name = $asset->key;
-			// is this an image asset or a template/snippet/config/layout file (the latter file types do not have public urls!)
-			if ($asset->public_url!==null)
-			{
-				//echo "hello";
-				// yes, this is an image, download it and save it
-			    $temp_file_contents = get_file($asset->public_url);
-			    write_file($temp_file_contents,$file_name);
-			}
-			else
-			{
-				// this is a text file of some sort. since it doesn't have a public url, we can't cURL it so the solution is to get the updated value of the file and overwrite the file in the local file structure
-				$response = get_data('/admin/smart_collections/'.$collection_id.'/order.json?asset[key]=snippets/new_file1.liquid'.'&theme_id='.$collection_id, $api_key, $password, $store_url, $collection_id);
-				//print_r($response);
-				file_put_contents($file_name, $response->asset->value);
-			}
-			// save the asset data we just retrieved to report on it below
-		    $updated_assets[] = $asset;
+		echo "curlPut";
+		//$url = 'https://' . $api_key . ':' . $password . '@' . $store_url;
+		//echo $url;
+		$url =  $url.$request;
+		$ch = curl_init(); //create a new cURL resource handle
+		curl_setopt($ch, CURLOPT_URL, $url); // Set URL to download
+		
+		$http_headers = array("Content-Type:application/json");
+		if ($access_token) {
+			$http_headers = array("Content-Type:application/json", "X-Shopify-Access-Token: $access_token");
 		}
+		
+		curl_setopt($ch, CURLOPT_HEADER, false); // Include header in result? (0 = yes, 1 = no)
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $http_headers);
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		
+		if ($data) {
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+		}
+		
+		$output = curl_exec($ch); // Download the given URL, and return output
+		
+		if ($output === false) {
+			return 'Curl error: ' . curl_error($ch);
+		}
+		
+		curl_close($ch); // Close the cURL resource, and free system resources
+		
+		return json_decode($output);
 	}
-	// finally, update the timestamp with the newest timestamp retrieved in the assets array
-	update_last_sync($new_last_updated_at, $api_key, $password, $store_url, $collection_id);
-	// deets
-	//echo '<h3>The following files were updated:</h3>';
-	//echo '<pre>';
-	//print_r($updated_assets);
-	//echo '</pre>';
+	
+	function updateSmartCollections($shop, $access_token,$cdata)
+	{
+		$curl_url = "https://$shop/admin/smart_collections/order.json?collection_id=345548033";		
+		return $this->curlPutRequest($curl_url, $access_token,$data);
+	}
+	
+		
 ?>
