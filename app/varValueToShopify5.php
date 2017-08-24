a788daf164c7200e44a8c70a6d61b2bf952fa616
prashant@acmeintech.in <prashant@acmeintech.in>
2017-08-24 15:17:26 +0530

    phpToShopify

--- app/varValueToShopify5.php
+++ app/varValueToShopify5.php
@@ -122,7 +122,7 @@ $theme_id = '143487233';
 			}
 			else
 			{
-	
+				echo "else";
 				$response = get_data('/admin/themes/'.$theme_id.'/assets.json?asset[key]=snippets/new_file2.liquid'.'&theme_id='.$theme_id, $api_key, $password, $store_url, $theme_id);
 	
 				file_put_contents($file_name, $response->asset->value);		    	
