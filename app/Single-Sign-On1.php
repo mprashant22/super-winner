d734b3ea34b350a0c4597754f7c65557d0b17c71
prashant@acmeintech.in <prashant@acmeintech.in>
2017-08-25 12:14:14 +0530

    single-sign-on

---
+++ app/Single-Sign-On1.php
@@ -0,0 +1,25 @@
+<link rel="stylesheet" href="../templates/vendor/bootstrap/css/bootstrap.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
+<link rel="stylesheet" href="../templates/vendor/bootstrap-social/bootstrap-social.css">
+<link rel="stylesheet" href="../templates/vendor/bootstrap-social/bootstrap-social.less">
+<link rel="stylesheet" href="../templates/vendor/bootstrap-social/bootstrap-social.scss">
+<?php
+echo "Prashant";
+
+
+?>
+
+<div style="width: 360px;">
+
+<a href="" class="btn btn-block btn-social btn-google"><i class="fa fa-google" aria-hidden="true"></i>Sign in with Google</a>
+
+<a href="" class="btn btn-block btn-social btn-facebook"><i class="fa fa-facebook" aria-hidden="true"></i>Sign in with Facebook</a>
+
+<a href="" class="btn btn-block btn-social btn-twitter"><i class="fa fa-twitter" aria-hidden="true"></i>Sign in with Twitter</a>
+
+<a href="" class="btn btn-block btn-social btn-instagram"><i class="fa fa-instagram" aria-hidden="true"></i>Sign in with Instagram</a>
+
+<a href="" class="btn btn-block btn-social btn-tumblr"><i class="fa fa-tumblr" aria-hidden="true"></i>Sign in with Tumblr</a>
+
+
+</div>
