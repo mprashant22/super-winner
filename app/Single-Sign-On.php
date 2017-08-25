3214c56a9b90d7e120802934710b4951a9babd80
prashant@acmeintech.in <prashant@acmeintech.in>
2017-08-25 12:19:31 +0530

    single-sign-on

--- app/Single-Sign-On.php
+++ app/Single-Sign-On.php
@@ -1,8 +1,7 @@
 <link rel="stylesheet" href="../templates/vendor/bootstrap/css/bootstrap.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
 <link rel="stylesheet" href="../templates/vendor/bootstrap-social/bootstrap-social.css">
-<link rel="stylesheet" href="../templates/vendor/bootstrap-social/bootstrap-social.less">
-<link rel="stylesheet" href="../templates/vendor/bootstrap-social/bootstrap-social.scss">
+
 <?php
 echo "Prashant";
 
