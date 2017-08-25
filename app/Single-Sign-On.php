1a2f8a459dd1f068cdb4a34a2d2d723ffc301767
prashant@acmeintech.in <prashant@acmeintech.in>
2017-08-25 11:33:42 +0530

    single-sign-on

--- app/Single-Sign-On.php
+++ app/Single-Sign-On.php
@@ -3,5 +3,47 @@ echo "Prashant";
 
 
 ?>
-
+<style type="text/css">
+.btn-facebook {
+    color: #fff;
+    background-color: #3b5998;
+    border-color: rgba(0,0,0,0.2);
+}
+bootstrap-social.css:10
+.btn-social {
+    position: relative;
+    padding-left: 44px;
+    text-align: left;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+}
+bootstrap.css:3455
+.btn-block {
+    display: block;
+    width: 100%;
+}
+bootstrap.css:3003
+.btn {
+    display: inline-block;
+    padding: 6px 12px;
+    margin-bottom: 0;
+    font-size: 14px;
+    font-weight: normal;
+    line-height: 1.42857143;
+    text-align: center;
+    white-space: nowrap;
+    vertical-align: middle;
+    -ms-touch-action: manipulation;
+    touch-action: manipulation;
+    cursor: pointer;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+    background-image: none;
+    border: 1px solid transparent;
+    border-radius: 4px;
+}
+</style>
 <a href="" class="btn btn-block btn-social btn-facebook">facebook</a>
