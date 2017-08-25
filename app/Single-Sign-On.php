d49c815d9399471ee2763bd2f2746c5621cb9c84
prashant@acmeintech.in <prashant@acmeintech.in>
2017-08-25 12:00:54 +0530

    single-sign-on

--- app/Single-Sign-On.php
+++ app/Single-Sign-On.php
@@ -17,5 +17,9 @@ echo "Prashant";
 
 <a href="" class="btn btn-block btn-social btn-twitter"><i class="fa fa-twitter" aria-hidden="true"></i>Sign in with Twitter</a>
 
+<a href="" class="btn btn-block btn-social btn-twitter"><i class="fa fa-twitter" aria-hidden="true"></i>Sign in with Twitter</a>
+
+<a href="" class="btn btn-block btn-social btn-tumblr"><i class="fa fa-tumblr" aria-hidden="true"></i>Sign in with Tumblr</a>
+
 
 </div>
