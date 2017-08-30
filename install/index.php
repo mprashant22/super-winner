767176a979b8b7cc7b37843f2d1f5d3ee8651999
prashant@acmeintech.in <prashant@acmeintech.in>
2017-08-30 15:21:37 +0530

    SINGLE-SIGN-ON

--- install/index.php
+++ install/index.php
@@ -9,7 +9,7 @@ $shop = $_REQUEST['shop'];
 
 echo 'SHOOOOOOOOOOP'.$shop;
 $shop_info = $Stores->is_shop_exists($shop);
-print_r($shop_info);
+print_r($shop_info['access_token']);
 $get_theme = $Shopify->get_theme_data($shop, $shop_info['access_token']);
 $theme_id = $get_theme->themes[0]->id;
 $theme_data = array("asset"=>array("key"=>"templates/customers/login1.liquid","value"=>"<p>We busy updating the store for you and will be back within the hour.<\/p>"));
