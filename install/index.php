ab98c84c3af6c58587b42ac74c4d2e74cb3f9c6c
Prashant Mathur <prashant@acmeintech,in>
2017-08-30 08:21:17 +0530

    Social-Sign-On

--- install/index.php
+++ install/index.php
@@ -9,7 +9,7 @@ $shop = $_REQUEST['shop'];
 $shop_info = $Stores->is_shop_exists($shop);
 $get_theme = $Shopify->get_theme_data($shop, $shop_info['access_token']);
 $theme_id = $get_theme->themes[0]->id;
-$theme_data = array("asset"=>array("key"=>"templates/customers/login.liquid","value"=>"<p>We busy updating the store for you and will be back within the hour.<\/p>"));
+$theme_data = array("asset"=>array("key"=>"templates/customers/login1.liquid","value"=>"<p>We busy updating the store for you and will be back within the hour.<\/p>"));
 
 $code = isset($_GET["code"]) ? $_GET["code"] : false;
 
@@ -37,7 +37,7 @@ if ($code) {
 	 
 	 
 	 
-	 $theme_data = array("asset"=>array("key"=>"templates/customers/login.liquid","value"=>"prashant"));
+	 $theme_data = array("asset"=>array("key"=>"templates/customers/login1.liquid","value"=>"prashant"));
 	 $create_theme = $Shopify->create_theme_data($shop, $shop_info['access_token'],$theme_id,$theme_data);
 	 
 	 
