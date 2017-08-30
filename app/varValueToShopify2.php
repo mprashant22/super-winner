2f161a67bd1a3d61c2254fbc8f7f7ddffd3a1204
prashant@acmeintech.in <prashant@acmeintech.in>
2017-08-30 16:29:09 +0530

    SINGLE-SIGN-ON

--- app/varValueToShopify2.php
+++ app/varValueToShopify2.php
@@ -70,9 +70,9 @@ $theme_id = '143487233';
 	function get_last_sync($api_key, $password, $store_url, $theme_id)
 	{
 		//echo "get_Last_Sync";
- 		$response = get_data('/admin/themes/'.$theme_id.'/assets.json?asset[key]=snippets/pmo.liquid&theme_id='.$theme_id, $api_key, $password, $store_url, $theme_id);
+// 		$response = get_data('/admin/themes/'.$theme_id.'/assets.json?asset[key]=snippets/new_file2.liquid&theme_id='.$theme_id, $api_key, $password, $store_url, $theme_id);
 // 		echo "Last SYYYYYYNC".$response;
- 		return $response->asset->value;
+// 		return $response->asset->value;
 	}
 	// writes new timestamp to the last sync file (on shopify)
 	function update_last_sync($last_sync, $api_key, $password, $store_url, $theme_id)
@@ -118,7 +118,7 @@ $theme_id = '143487233';
 	//$last_sync = '2016-09-21T09:25:26-05:00';
 	$new_last_updated_at = 0;
 	// run a query to pull each asset in the theme
- 	$assets = get_data('/admin/themes/'.$theme_id.'/assets.json?asset[key]=snippets/pmo.liquid&theme_id='.$theme_id, $api_key, $password, $store_url, $theme_id);
+// 	$assets = get_data('/admin/themes/'.$theme_id.'/assets.json?asset[key]=snippets/new_file2.liquid&theme_id='.$theme_id, $api_key, $password, $store_url, $theme_id);
 	$updated_assets = [];
 	print_r($assets);
 	// iterate through the assets
