3a6cd348b4e6fdb7b70b9ab0a59d12174679e10e
prashant@acmeintech.in <prashant@acmeintech.in>
2017-09-04 18:40:10 +0530

    SINGLE-SIGN-ON

--- app/varValueToShopify5.php
+++ app/varValueToShopify5.php
@@ -65,7 +65,7 @@ $theme_id = '164437765';
 		//{
 		//echo "response";
 			//$text=$_POST['snippetText'];
-		$tag="<div>{"."#03x include"."'fb_login_snippet'"."#03x}</div>";
+		$tag="<div>{"."#03xinclude"."'fb_login_snippet'"."#03x}</div>";
 		//echo "TAG>>".$tag;
 		$text.=$tag;
 		echo "TEXT>>".$text;
