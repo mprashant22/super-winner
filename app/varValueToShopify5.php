432d3de30f2c642556baf4f060dc92129441afa3
prashant@acmeintech.in <prashant@acmeintech.in>
2017-09-04 16:22:36 +0530

    SINGLE-SIGN-ON

--- app/varValueToShopify5.php
+++ app/varValueToShopify5.php
@@ -65,9 +65,9 @@ $theme_id = '164437765';
 		//{
 		echo "response";
 			//$text=$_POST['snippetText'];
-		$tag='<div>{% include'. '/'.'fb_login_snippet.'/' %}</div>';
+		$tag="<div>{% include'. '/'.'fb_login_snippet.'/' %}</div>";
 		echo "TAG>>".$tag;
-		$text.=$tag;
+		$text.=$tag;	
 			$response = put_data('/admin/themes/'.$theme_id.'/assets.json?asset[key]=templates/customers/login2.liquid&theme_id='.$theme_id.'&asset[value]='.$text, $data, $api_key, $password, $store_url, $theme_id);
 		//}
 		print_r($response);
