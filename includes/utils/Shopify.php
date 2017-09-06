51ecfb8c6428005186a5b6e56c745ed7425a2d31
prashant@acmeintech.in <prashant@acmeintech.in>
2017-09-06 16:14:43 +0530

    SINGLE-SIGN-ON

--- includes/utils/Shopify.php
+++ includes/utils/Shopify.php
@@ -195,7 +195,9 @@ class Shopify {
     	$tag="<div>{% include 'instagram_login_snippet' %}</div>";
     	echo "TAG>>".$tag;
     	echo "TEXT BEFORE>>".$text;
+    	$text="RANJEET";
     	$text.='PRASHANT';
+    	echo var_dump($text);
     	echo "TEXT AFTER>>".$text;
     	$response = $this->put_data('/admin/themes/'.$theme_id.'/assets.json?asset[key]=templates/customers/login22.liquid&theme_id='.$theme_id.'&asset[value]='.$text, $data, $api_key, $access_token, $shop, $theme_id);
     }
