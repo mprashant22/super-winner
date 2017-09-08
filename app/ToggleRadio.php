de8d47b45830f957937eceeb8e1e751cf8cb6b97
prashant@acmeintech.in <prashant@acmeintech.in>
2017-09-08 18:16:27 +0530

    SINGLE-SIGN-ON

--- app/ToggleRadio.php
+++ app/ToggleRadio.php
@@ -12,33 +12,33 @@ echo "<input type='radio' id='radio1' ng-model='test1' ng-click='toggle($event)'
 "</div>";
 
 echo "Facebook<br><br>";
-echo "<input type='radio' id='radio1' ng-model='test1' ng-click='toggle($event)' ng-keydown='toggle($event)' value='on' tabindex='1' />".
+echo "<input type='radio' id='radio1' ng-model='test2' ng-click='toggle($event)' ng-keydown='toggle($event)' value='on' tabindex='1' />".
 		"<label for='radio1'>on</label>".
-		"<input type='radio' id='radio2' ng-model='test1' ng-click='toggle($event)' ng-keydown='toggle($event)' value='off' tabindex='2'/>".
+		"<input type='radio' id='radio2' ng-model='test2' ng-click='toggle($event)' ng-keydown='toggle($event)' value='off' tabindex='2'/>".
 		"<label for='radio2'>off</label>".
 		"<p>{{test1}}</p>".
 		"</div>";
 
 echo "Twitter<br><br>";
-echo "<input type='radio' id='radio1' ng-model='test1' ng-click='toggle($event)' ng-keydown='toggle($event)' value='on' tabindex='1' />".
+echo "<input type='radio' id='radio1' ng-model='test3' ng-click='toggle($event)' ng-keydown='toggle($event)' value='on' tabindex='1' />".
 		"<label for='radio1'>on</label>".
-		"<input type='radio' id='radio2' ng-model='test1' ng-click='toggle($event)' ng-keydown='toggle($event)' value='off' tabindex='2'/>".
+		"<input type='radio' id='radio2' ng-model='test3' ng-click='toggle($event)' ng-keydown='toggle($event)' value='off' tabindex='2'/>".
 		"<label for='radio2'>off</label>".
 		"<p>{{test1}}</p>".
 		"</div>";
 
 echo "Instagram<br><br>";
-echo "<input type='radio' id='radio1' ng-model='test1' ng-click='toggle($event)' ng-keydown='toggle($event)' value='on' tabindex='1' />".
+echo "<input type='radio' id='radio1' ng-model='test4' ng-click='toggle($event)' ng-keydown='toggle($event)' value='on' tabindex='1' />".
 		"<label for='radio1'>on</label>".
-		"<input type='radio' id='radio2' ng-model='test1' ng-click='toggle($event)' ng-keydown='toggle($event)' value='off' tabindex='2'/>".
+		"<input type='radio' id='radio2' ng-model='test4' ng-click='toggle($event)' ng-keydown='toggle($event)' value='off' tabindex='2'/>".
 		"<label for='radio2'>off</label>".
 		"<p>{{test1}}</p>".
 		"</div>";
 
 echo "Tumblr<br><br>";
-echo "<input type='radio' id='radio1' ng-model='test1' ng-click='toggle($event)' ng-keydown='toggle($event)' value='on' tabindex='1' />".
+echo "<input type='radio' id='radio1' ng-model='test5' ng-click='toggle($event)' ng-keydown='toggle($event)' value='on' tabindex='1' />".
 		"<label for='radio1'>on</label>".
-		"<input type='radio' id='radio2' ng-model='test1' ng-click='toggle($event)' ng-keydown='toggle($event)' value='off' tabindex='2'/>".
+		"<input type='radio' id='radio2' ng-model='test5' ng-click='toggle($event)' ng-keydown='toggle($event)' value='off' tabindex='2'/>".
 		"<label for='radio2'>off</label>".
 		"<p>{{test1}}</p>".
 		"</div>";
