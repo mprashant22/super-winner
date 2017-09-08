23a51bb90f8c47ef67b5b7d327f7da97e9b0ca77
prashant@acmeintech.in <prashant@acmeintech.in>
2017-09-08 14:10:34 +0530

    SINGLE-SIGN-ON

---
+++ app/ToggleRadio.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+<link href="css/bootstrap-switch/bootstrap-switch.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+    <main id="content" role="main">
+
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-6">
+              <h3 class="h5">A demo of radio buttons as switch</h3>
+              <input type="radio" name="radio1" checked data-on-color="success" data-off-color="warning" class="switch-radio1">
+              <input type="radio" name="radio1" data-on-color="danger" data-off-color="warning" class="switch-radio1">
+              <input type="radio" name="radio1" data-on-color="info" data-off-color="primary"  class="switch-radio1">
+            </div>
+
+  
+        </div>
+
+    </main>
+ 
+
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script> 
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <script src="js/bootstrap-switch/highlight.js"></script>
+    <script src="js/bootstrap-switch/bootstrap-switch.js"></script>
+    <script src="js/bootstrap-switch/main.js"></script>
+</body>
+</html>
