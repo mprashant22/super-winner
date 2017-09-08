e79e843eb9a9307d20dcc9bdfb13f04a53c7e564
prashant@acmeintech.in <prashant@acmeintech.in>
2017-09-08 15:03:07 +0530

    SINGLE-SIGN-ON

--- app/ToggleRadio.php
+++ app/ToggleRadio.php
@@ -1,36 +1,54 @@
-<!DOCTYPE html>
-<html>
-<head>
-
-<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-<link href="css/bootstrap-switch/bootstrap-switch.css" rel="stylesheet">
-
-</head>
-
-<body>
-
-    <main id="content" role="main">
-
-      <div class="container">
-        <div class="row">
-          <div class="col-sm-6">
-              <h3 class="h5">A demo of radio buttons as switch</h3>
-              <input type="radio" name="radio1" checked data-on-color="success" data-off-color="warning" class="switch-radio1">
-              <input type="radio" name="radio1" data-on-color="danger" data-off-color="warning" class="switch-radio1">
-              <input type="radio" name="radio1" data-on-color="info" data-off-color="primary"  class="switch-radio1">
-            </div>
-
-  
-        </div>
-
-    </main>
- 
+<?php ?>
 
 
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script> 
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-    <script src="js/bootstrap-switch/highlight.js"></script>
-    <script src="js/bootstrap-switch/bootstrap-switch.js"></script>
-    <script src="js/bootstrap-switch/main.js"></script>
-</body>
-</html>
+<div ng-app ng-controller="Example">
+    
+    <!-- First pair of radio buttons -->
+    <input type="radio" id="radio1" ng-model="test1"
+           ng-click="toggle($event)" 
+           ng-keydown="toggle($event)"
+           value="one" tabindex="1" />
+    <label for="radio1">One</label>
+    <input type="radio" id="radio2" ng-model="test1"
+           ng-click="toggle($event)"
+           ng-keydown="toggle($event)"
+           value="two" tabindex="2"/>
+    <label for="radio2">Two</label>
+    <p>{{test1}}</p>
+    
+    </div>
+    
+    <script>
+    function Example($scope) {
+        
+        /*
+         * When an element is clicked, the model has not yet
+         * been updated, so we check the value of the model 
+         * to the value of the clicked element.  If they are 
+         * identical, then the user is clicking the "current"
+         * (selected) option and we can toggle it by setting 
+         * the model to null.
+         */
+        $scope.toggle = function(event) {
+            var keyboardEvent = event.type == 'keydown'
+            var spaceOrEnterKey = keyboardEvent &&
+                                (event.which == 13 ||
+                                 event.which == 32)
+            var elem = event.target
+            var modelKey = angular.element(elem).attr('ng-model')
+            if (elem.value == $scope[modelKey]
+               && (!keyboardEvent || spaceOrEnterKey)) {
+                $scope[modelKey] = null
+            } else {
+                if (spaceOrEnterKey)
+                    $scope[modelKey] = elem.value
+            }
+            if (spaceOrEnterKey)
+                event.preventDefault()
+        }
+
+        // Demonstrate radio button with default selection
+        $scope.test2 = 'two'
+    }
+
+    </script>
