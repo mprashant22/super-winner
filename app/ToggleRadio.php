f9aba008cc1163b43dc9127f6084336760591855
prashant@acmeintech.in <prashant@acmeintech.in>
2017-09-08 18:36:15 +0530

    SINGLE-SIGN-ON

--- app/ToggleRadio.php
+++ app/ToggleRadio.php
@@ -45,48 +45,11 @@ echo "<input type='radio' id='radio1' ng-model='test5' ng-click='toggle($event)'
 
 ?>
 
-
-
-    
-
-    
-    
-    
-    
-    
-
-    
+ 
     <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.1/angular.min.js"></script>
     <script type="text/javascript">
-    function Example($scope) {
-        
-        /*
-         * When an element is clicked, the model has not yet
-         * been updated, so we check the value of the model 
-         * to the value of the clicked element.  If they are 
-         * identical, then the user is clicking the "current"
-         * (selected) option and we can toggle it by setting 
-         * the model to null.
-         */
-        $scope.toggle = function(event) {
-            var keyboardEvent = event.type == 'keydown'
-            var spaceOrEnterKey = keyboardEvent &&
-                                (event.which == 13 ||
-                                 event.which == 32)
-            var elem = event.target
-            var modelKey = angular.element(elem).attr('ng-model')
-            if (elem.value == $scope[modelKey]
-               && (!keyboardEvent || spaceOrEnterKey)) {
-                $scope[modelKey] = null
-            } else {
-                if (spaceOrEnterKey)
-                    $scope[modelKey] = elem.value
-            }
-            if (spaceOrEnterKey)
-                event.preventDefault()
-        }
 
-        // Demonstrate radio button with default selection
-        $scope.test2 = 'off'
-    }
+    angular.module("Example", []).controller("Example", ["$scope", function($scope) {
+        $scope.test2 = "one"
+    }])
     </script>
