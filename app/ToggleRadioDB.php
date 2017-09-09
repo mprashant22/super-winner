a5effe664a957e48cb52305f4cf58249a5f0388c
prashant@acmeintech.in <prashant@acmeintech.in>
2017-09-09 17:40:39 +0530

    SINGLE-SIGN-ON

--- app/ToggleRadioDB.php
+++ app/ToggleRadioDB.php
@@ -1,1 +1,9 @@
-rfujhry
+<?php
+
+$postdata = file_get_contents("php://input");
+    $request = json_decode($postdata);
+    echo $request;
+    //@$x = $request->email;    
+    //echo $x; //this will go back u
+    
+    ?>
