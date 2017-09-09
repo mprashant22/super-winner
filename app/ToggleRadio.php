f7244c493fce73929b9c446b93a7166d2266cf0c
prashant@acmeintech.in <prashant@acmeintech.in>
2017-09-09 11:47:11 +0530

    SINGLE-SIGN-ON

--- app/ToggleRadio.php
+++ app/ToggleRadio.php
@@ -11,77 +11,77 @@
 
 <?php 
  
+ echo "<div ng-app='myApp' ng-controller='Example'>";
+
+ // First pair of radio buttons
+ echo "Google ";
+ echo "<input type='radio' id='radio1' ng-model='test1' value='on' tabindex='1' />.
+ <label for='radio1'>on</label>.
+ <input type='radio' id='radio2' ng-model='test1' value='off' tabindex='2'/>.
+ <label for='radio2'>off</label>.
+ <p>{{test1}}</p>.
+ </div>";
+
+ echo "Facebook<br><br>";
+ echo "<input type='radio' id='radio3' ng-model='test2' value='on' />.
+ 		<label for='radio3'>on</label>.
+ 		<input type='radio' id='radio4' ng-model='test2' value='off'/>.
+ 		<label for='radio4'>off</label>.
+ 		<p>{{test2}}</p>.
+ 		</div>";
+
+ echo "Twitter<br><br>";
+ echo "<input type='radio' id='radio5' ng-model='test3' value='on'/>.
+ 		<label for='radio5>on</label>.
+ 		<input type='radio' id='radio6' ng-model='test3' value='off'/>.
+ 		<label for='radio6'>off</label>.
+ 		<p>{{test3}}</p>.
+ 		</div>";
+
+ echo "Instagram<br><br>";
+ echo "<input type='radio' id='radio7' ng-model='test4' value='on' />.
+ 		<label for='radio7'>on</label>.
+ 		<input type='radio' id='radio8' ng-model='test4' value='off'/>.
+ 		<label for='radio8'>off</label>.
+ 		<p>{{test4}}</p>.
+ 		</div>";
+
+ echo "Tumblr<br><br>";
+ echo "<input type='radio' id='radio9' ng-model='test5' value='on' />.
+ 		<label for='radio9'>on</label>.
+ 		<input type='radio' id='radio10' ng-model='test5' value='off' />.
+ 		<label for='radio10'>off</label>.
+ 		<p>{{test5}}</p>.
+ 		</div>";
+
+?>
+// <?php 
 // echo "<div ng-app='myApp' ng-controller='Example'>";
-
-// // First pair of radio buttons
-// echo "Google ";
-// echo "<input type='radio' id='radio1' ng-model='test1' value='on' tabindex='1' />".
-// "<label for='radio1'>on</label>".
-// "<input type='radio' id='radio2' ng-model='test1' value='off' tabindex='2'/>".
-// "<label for='radio2'>off</label>".
-// "<p>{{test1}}</p>".
-// "</div>";
-
-// echo "Facebook<br><br>";
-// echo "<input type='radio' id='radio3' ng-model='test2' value='on' />".
-// 		"<label for='radio3'>on</label>".
-// 		"<input type='radio' id='radio4' ng-model='test2' value='off'/>".
-// 		"<label for='radio4'>off</label>".
-// 		"<p>{{test2}}</p>".
-// 		"</div>";
-
-// echo "Twitter<br><br>";
-// echo "<input type='radio' id='radio5' ng-model='test3' value='on'/>".
-// 		"<label for='radio5>on</label>".
-// 		"<input type='radio' id='radio6' ng-model='test3' value='off'/>".
-// 		"<label for='radio6'>off</label>".
-// 		"<p>{{test3}}</p>".
-// 		"</div>";
-
-// echo "Instagram<br><br>";
-// echo "<input type='radio' id='radio7' ng-model='test4' value='on' />".
-// 		"<label for='radio7'>on</label>".
-// 		"<input type='radio' id='radio8' ng-model='test4' value='off'/>".
-// 		"<label for='radio8'>off</label>".
-// 		"<p>{{test4}}</p>".
-// 		"</div>";
-
-// echo "Tumblr<br><br>";
-// echo "<input type='radio' id='radio9' ng-model='test5' value='on' />".
-// 		"<label for='radio9'>on</label>".
-// 		"<input type='radio' id='radio10' ng-model='test5' value='off' />".
-// 		"<label for='radio10'>off</label>".
-// 		"<p>{{test5}}</p>".
-// 		"</div>";
-
-?>
-<?php 
-echo "<div ng-app='myApp' ng-controller='Example'>";
     
-    // First pair of radio buttons
-    echo "<input type='radio' id='radio1' ng-model='test1' value='one' tabindex='1' />.
-    <label for='radio1'>One</label>.
-    <input type='radio' id='radio2' ng-model='test1' value='two' tabindex='2'/>.
-    <label for='radio2'>Two</label>.
-    <a class='undo' ng-click='test1 = null'>&otimes;</a>.
-    <p>{{test1}}</p>";
+//     // First pair of radio buttons
+//     echo "<input type='radio' id='radio1' ng-model='test1' value='one' tabindex='1' />.
+//     <label for='radio1'>One</label>.
+//     <input type='radio' id='radio2' ng-model='test1' value='two' tabindex='2'/>.
+//     <label for='radio2'>Two</label>.
+//     <a class='undo' ng-click='test1 = null'>&otimes;</a>.
+//     <p>{{test1}}</p>";
     
 
-    // Second pair of radio buttons 
-    echo "<input type='radio' id='radio3' ng-model='test2' value='one' checked='checked' />.
-    <label for='radio3'>One</label>.
-    <input type='radio' id='radio4' ng-model='test2' value='two' />.
-    <label for='radio4'>Two</label>.
-    <a class='undo' ng-click='test2 = null'>&otimes;</a>.
-    <p>{{test2}}</p>";		
+//     // Second pair of radio buttons 
+//     echo "<input type='radio' id='radio3' ng-model='test2' value='one' checked='checked' />.
+//     <label for='radio3'>One</label>.
+//     <input type='radio' id='radio4' ng-model='test2' value='two' />.
+//     <label for='radio4'>Two</label>.
+//     <a class='undo' ng-click='test2 = null'>&otimes;</a>.
+//     <p>{{test2}}</p>";		
     
-    // Second pair of radio buttons
-    echo "<input type='radio' id='radio5' ng-model='test3' value='one' checked='checked' />.
-    <label for='radio5'>One</label>.
-    <input type='radio' id='radio6' ng-model='test3' value='two' />.
-    <label for='radio6'>Two</label>.
-    <a class='undo' ng-click='test3 = null'>&otimes;</a>.
-    <p>{{test3}}</p>";
+//     // Second pair of radio buttons
+//     echo "<input type='radio' id='radio5' ng-model='test3' value='one' checked='checked' />.
+//     <label for='radio5'>One</label>.
+//     <input type='radio' id='radio6' ng-model='test3' value='two' />.
+//     <label for='radio6'>Two</label>.
+//     <a class='undo' ng-click='test3 = null'>&otimes;</a>.
+//     <p>{{test3}}</p>";
     
-    echo "</div>";
-?>
+//     echo "</div>";
+// ?>
