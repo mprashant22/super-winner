a496d8662cb46ea4effeeff221bbee788198dfdf
prashant@acmeintech.in <prashant@acmeintech.in>
2017-09-09 12:14:37 +0530

    SINGLE-SIGN-ON

--- app/ToggleRadio.php
+++ app/ToggleRadio.php
@@ -2,6 +2,7 @@
     <script type="text/javascript">
     angular.module("myApp", []).controller("Example", ["$scope", function($scope) {
         $scope.test2 = "one"
+            alert("PRASHANT");
     }])
     </script>
 
