d60b87d0496ef41e87734e077f6fab08937f3deb
Prashant Mathur <prashant@acmeintech,in>
2017-09-12 00:52:57 +0530

    Social-Sign-On

--- app/ToggleRadioDB.php
+++ app/ToggleRadioDB.php
@@ -22,7 +22,7 @@ class SocialLoginSelect extends DB_Connection
     	$sql = "INSERT into '".$this->table_name."'(storeName, optionGoogle, `optionFacebook`,`optionTwitter`, `optionInstagram`, `optionTumblr`) values('".
     $request[0]."',".$request[1].",".$request[2].",".$request[3].",".$request[4].",".$request[5].")";
     	echo $sql;
-//mysqli_query($this->connection,$sql) or die(mysqli_error($this->connection));
+mysqli_query($this->connection,$sql) or die(mysqli_error($this->connection));
     	//$shop = $_REQUEST['shop'];
     	//echo "dukaan".$shop;
 		//echo $sql.">>".
