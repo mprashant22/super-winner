4c99db9c59dce108f31a3876cb85064b8bd19eb8
prashant@acmeintech.in <prashant@acmeintech.in>
2017-09-11 16:18:02 +0530

    SINGLE-SIGN-ON

--- app/ToggleRadioDB.php
+++ app/ToggleRadioDB.php
@@ -13,14 +13,16 @@ class SocialLoginSelect extends DB_Connection
     {
     	$postdata = file_get_contents("php://input");
 		$request = json_decode($postdata);
+		echo var_dump($request);
     	
     	
-    	$sql = "INSERT into `".$table_name."`(storeName, optionGoogle, `optionFacebook`,`optionTwitter`, `optionInstagram`, `optionTumblr`) values('".$request[0]."','".$request[1]."','".$request[2]."','".$request[3]."','".$request[4]."','".$request[5]."')";
-mysqli_query($this->connection,$sql) or die(mysqli_error($this->connection));	
-    	//$shop = $_REQUEST['shop'];
-    	//echo "dukaan".$shop;
-		//echo $sql.">>".
-    	print_r($request);
+//     	$sql = "INSERT into `".$table_name."`(storeName, optionGoogle, `optionFacebook`,`optionTwitter`, `optionInstagram`, `optionTumblr`) values('".
+//     	$request[0]."','".$request[1]."','".$request[2]."','".$request[3]."','".$request[4]."','".$request[5]."')";
+// mysqli_query($this->connection,$sql) or die(mysqli_error($this->connection));	
+//     	//$shop = $_REQUEST['shop'];
+//     	//echo "dukaan".$shop;
+// 		//echo $sql.">>".
+    	//print_r($request);
     }
 }
 $obj=new SocialLoginSelect();
