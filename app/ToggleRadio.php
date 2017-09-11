84963a551cdd641a96d8f8c00a6b96af45a454ed
prashant@acmeintech.in <prashant@acmeintech.in>
2017-09-11 19:03:44 +0530

    SINGLE-SIGN-ON

--- app/ToggleRadio.php
+++ app/ToggleRadio.php
@@ -17,39 +17,39 @@ $shop = "mathurs-store.myshopify.com";
 // echo "<input type='hidden' name='shop' ng-init="shop='prashant'" ng-value='shop'>";
  // First pair of radio buttons
  echo "Google";
- echo "<input type='radio' id='radio1' ng-model='test1' value='on' tabindex='1' />
+ echo "<input type='radio' id='radio1' ng-model='test1' value=1 tabindex='1' />
  <label for='radio1'>on</label>
- <input type='radio' id='radio2' ng-model='test1' value='off' tabindex='2'/>
+ <input type='radio' id='radio2' ng-model='test1' value=0 tabindex='2'/>
  <label for='radio2'>off</label>
  <p>{{test1}}</p>";
  
 
  echo "Facebook";
- echo "<input type='radio' id='radio3' ng-model='test2' value='on' />
+ echo "<input type='radio' id='radio3' ng-model='test2' value=1 />
  		<label for='radio3'>on</label>
- 		<input type='radio' id='radio4' ng-model='test2' value='off'/>
+ 		<input type='radio' id='radio4' ng-model='test2' value=0/>
  		<label for='radio4'>off</label>
  		<p>{{test2}}</p>";
 
  echo "Twitter";
- echo "<input type='radio' id='radio5' ng-model='test3' value='on'/>
+ echo "<input type='radio' id='radio5' ng-model='test3' value=1/>
  		<label for='radio5'>on</label>
- 		<input type='radio' id='radio6' ng-model='test3' value='off'/>
+ 		<input type='radio' id='radio6' ng-model='test3' value=0/>
  		<label for='radio6'>off</label>
  		<p>{{test3}}</p>";
  		
 
  echo "Instagram";
- echo "<input type='radio' id='radio7' ng-model='test4' value='on' />
+ echo "<input type='radio' id='radio7' ng-model='test4' value=1 />
  		<label for='radio7'>on</label>
- 		<input type='radio' id='radio8' ng-model='test4' value='off'/>
+ 		<input type='radio' id='radio8' ng-model='test4' value=0/>
  		<label for='radio8'>off</label>
  		<p>{{test4}}</p>"; 		
 
  echo "Tumblr";
- echo "<input type='radio' id='radio9' ng-model='test5' value='on' />
+ echo "<input type='radio' id='radio9' ng-model='test5' value=1 />
  		<label for='radio9'>on</label>
- 		<input type='radio' id='radio10' ng-model='test5' value='off' />
+ 		<input type='radio' id='radio10' ng-model='test5' value=0 />
  		<label for='radio10'>off</label>
  		<p>{{test5}}</p>
  		</div>";
