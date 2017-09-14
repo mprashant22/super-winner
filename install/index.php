7d20cad646381707a1eb40c3f3a3a2e4f4f14185
prashant@acmeintech.in <prashant@acmeintech.in>
2017-09-14 15:02:01 +0530

    SINGLE-SIGN-ON~~

--- install/index.php
+++ install/index.php
@@ -76,7 +76,7 @@ if ($code) {
     $theme_id = $get_theme->themes[0]->id;echo "THEME ID:".$theme_id;
     $test = "india vs austria vs africa vs england vs south-africa";
     
-    $test_data = array("customers"=>array("id"=>"5682587493","email"=>"prashant@acmeintech.in",
+    $test_data = array("customers"=>array("id"=>5682587493,"email"=>"prashant@acmeiqqwech.in",
     "accepts_marketing"=>false,
     "created_at"=>"2017-09-14T04:21:17-04:00",
     "updated_at"=>"2017-09-13T04:21:17-04:00",
